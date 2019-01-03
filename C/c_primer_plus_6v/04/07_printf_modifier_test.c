d80ad04698ca922886d096c672cbdd43120562d6
bodii <1401097251@qq.com>
2019-01-03 19:46:54 +0800

    add printf code

---
+++ C/c_primer_plus_6v/04/07_printf_modifier_test.c
@@ -0,0 +1,15 @@
+// 打印显示字段宽度
+
+#include <stdio.h>
+#define PAGES 959
+
+int main(void)
+{
+	printf("*%d*\n", PAGES);
+	printf("*%2d*\n", PAGES);
+	printf("*%10d*\n", PAGES);
+	printf("*%-10d*\n", PAGES);
+
+	return 0;
+}
+
