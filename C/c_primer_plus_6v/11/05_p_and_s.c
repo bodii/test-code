5aac5382b0afd7ffcbec4a54c02d343fdad60908
bodii <1401097251@qq.com>
2019-01-13 19:57:16 +0800

    add string code

---
+++ C/c_primer_plus_6v/11/05_p_and_s.c
@@ -0,0 +1,16 @@
+// 指针和字符串
+
+#include <stdio.h>
+
+int main(void)
+{
+	const char * mesg = "Don't be a fool!";
+	const char * copy;
+
+	copy = mesg;
+	printf("%s\n", copy);
+	printf("mesg = %s; &mesg = %p; value = %p\n", mesg, &mesg, mesg);
+	printf("copy = %s; &copy = %p; value = %p\n", copy, &copy, copy);
+
+	return 0;
+}
