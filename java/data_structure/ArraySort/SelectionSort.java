85b7bd8ff657ddefdff710c82d21c174a2a64a01
bodii <1401097251@qq.com>
2019-03-12 22:17:59 +0800

    add linear list selection sort code

--- java/data_structure/ArraySort/SelectionSort.java
+++ java/data_structure/ArraySort/SelectionSort.java
@@ -3,7 +3,44 @@
  */
 
 public class SelectionSort {
-    private static SequenceList sort(SequeneList s1, int placenum) {
+    private static SequenceList sort(SequenceList s1, int placenum) {
+        if (placenum >= s1.size()) {
+            return s1;
+        }
 
+        int head = Integer.parseInt(s1.get(placenum).toString());
+        int element = Integer.parseInt(s1.get(placenum).toString());
+        int elementPlace = 0;
+        for (int i = placenum; i < s1.size(); i++) {
+            if (Integer.parseInt(s1.get(i).toString()) < element) {
+                element = Integer.parseInt(s1.get(i).toString());
+                elementPlace = i;
+            }
+        }
+
+        if (elementPlace != 0) {
+            s1.set(placenum, element);
+            s1.set(elementPlace, head);
+        }
+
+        sort(s1, ++placenum);
+
+        return s1;
+    }
+
+    public static void main(String[] args) {
+        SequenceList s1 = new SequenceList();
+        s1.add(23);
+        s1.add(45);
+        s1.add(31);
+        s1.add(17);
+        s1.add(26);
+        s1.add(44);
+
+        SequenceList st = sort(s1, 0);
+        System.out.println("size: " + st.size());
+        for (int i = 0; i < st.size(); i++) {
+            System.out.println(st.get(i));
+        }
     }
 }
---
+++ java/data_structure/ArraySort/SequenceList.java
@@ -0,0 +1,95 @@
+/**
+ * 线性表顺序存储操作实现
+ */
+public class SequenceList implements LinearList {
+    private Object[] sList;
+    private int size;
+
+    public SequenceList(int length) {
+        if (length == 0) {
+            this.sList = new Object[10];
+        }
+        this.sList = new Object[length];
+    }
+
+    public SequenceList() {
+        this(10);
+    }
+
+    public boolean isEmpty() {
+        if (size <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(int index) {
+        checkIndex(index);
+
+        return (Object) sList[index];
+    }
+
+    public void set(int index, Object element) {
+        checkIndex(index);
+
+        sList[index] = element;
+    }
+
+    public boolean add(Object element) {
+        return add(size, element);
+    }
+
+    public boolean add(int index, Object element) {
+        checkIndex(index);
+        checkSize();
+
+        for (int i = size - 1; i >= index; i--) {
+            sList[i + 1] = sList[i];
+        }
+
+        sList[index] = element;
+        size++;
+
+        return true;
+    }
+
+    public Object remove(int index) {
+        checkIndex(index);
+
+        Object old = (Object) sList[index];
+        for (int i = index; i < size - 1; i++) {
+            sList[i] = sList[i + 1];
+        }
+        sList[--size] = null;
+        return old;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            sList[i] = null;
+        }
+
+        size = 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("index:  " + index + "  size:" + size);
+        }
+    }
+
+    private void checkSize() {
+        if (size == sList.length) {
+            Object[] temp = sList;
+            this.sList = new Object[temp.length * 2];
+            for (int i = 0; i < temp.length; i++) {
+                this.sList[i] = temp[i];
+            }
+        }
+    }
+}
