e45325ef749052d53e37332e0cfae7c178ad35b9
bodii <1401097251@qq.com>
2019-03-13 20:55:46 +0800

    add recursive code

---
+++ java/data_structure/Recursive/Recursive.java
@@ -0,0 +1,32 @@
+public class Recursive {
+
+	public static int factoria(int n)
+	{
+		int value = 1;
+		while(n > 0) {
+			value *= n--;
+		}
+
+		return value;
+	}
+
+	public static int recur(int n)
+	{
+		if (n == 0)
+			return 1;
+		else
+			return n*recur(n-1);
+	}
+
+	public static void main(String[] args)
+	{
+		// 测试
+		int v1 = factoria(10);
+		System.out.println("v1 is value: " + v1);
+
+		// 测试递归
+		int v2 = recur(10);
+		System.out.println("v2 is value: " + v2);
+
+	}
+}
