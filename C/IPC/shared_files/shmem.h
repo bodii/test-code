4c66a20bc9cb7a24344c30a5b65914067401eee6
bodii <1401097251@qq.com>
2019-06-21 15:52:55 +0800

    add shared file and shared memory code

---
+++ C/IPC/shared_files/shmem.h
@@ -0,0 +1,5 @@
+#define BackingFile  "/shMemEx"
+#define AccessPerms  0644
+#define ByteSize     512 
+#define SemaphoreName "semaphorename_01" 
+#define MemContents   "This is the way the world ends...\n"
