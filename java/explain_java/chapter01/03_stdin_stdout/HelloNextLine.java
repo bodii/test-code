4ca54c36de504ff9f90f5bac98f932ff323142c1
bodii <1401097251@qq.com>
2019-07-03 07:08:07 +0800

    add Scanner nextLine and String assign

---
+++ java/explain_java/chapter01/03_stdin_stdout/HelloNextLine.java
@@ -0,0 +1,14 @@
+// 读入姓名并打招呼
+
+import java.util.Scanner;
+
+class HelloNextLine {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("您的姓名是: ");
+		String s = stdIn.nextLine();      // 读入1行字符串
+
+		System.out.println("你好" + s + "先生");
+	}
+}
