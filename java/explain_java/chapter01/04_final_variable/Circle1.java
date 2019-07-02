d10089ef9ae841e1a509c64e0f429f676022f80f
bodii <1401097251@qq.com>
2019-07-02 17:56:39 +0800

    add final variable and random

---
+++ java/explain_java/chapter01/04_final_variable/Circle1.java
@@ -0,0 +1,15 @@
+// 计算圆的周长和面积（其1： 使用浮点型常量来表示圆周率)
+
+import java.util.Scanner;
+
+class Circle1 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("半径: ");
+		double r = stdIn.nextDouble();
+
+		System.out.println("周长是" + 2 * 3.14 * r + "。");
+		System.out.println("面积是" + 3.14 * r * r + "。");
+	}
+}
