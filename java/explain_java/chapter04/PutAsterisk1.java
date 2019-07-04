389ca86fca4b44ef6221dd9dd89df89ff6719214
bodii <1401097251@qq.com>
2019-07-04 18:00:24 +0800

    add while statement

---
+++ java/explain_java/chapter04/PutAsterisk1.java
@@ -0,0 +1,19 @@
+// 显示所读入的数值个*
+
+import java.util.Scanner;
+
+class PutAsterisk1 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("要显示多少个*呢: ");
+		int n = stdIn.nextInt();
+
+		int i = 0;
+		while (i < n) {
+			System.out.print('*');
+			i++;
+		}
+		System.out.println();
+	}
+}
