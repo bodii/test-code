cfe27376ae1124998d14a8b26264eb94242588a6
bodii <1401097251@qq.com>
2019-07-24 22:06:52 +0800

    add interfface use

---
+++ java/explain_java/chapter14/shape3/PortablePlayerTester.java
@@ -0,0 +1,11 @@
+// 类PortablePlayer使用示例
+
+class PortablePlayerTester {
+	
+	public static void main(String[] args) {
+		PortablePlayer a = new PortablePlayer();
+		a.play();
+		a.stop();
+		a.changeSkin(Skinnable.LEOPARD);
+	}
+}
---
+++ java/explain_java/chapter14/shape3/Skinnable.java
@@ -0,0 +1,10 @@
+// 换肤接口
+
+public interface Skinnable {
+	int BLACK = 0;
+	int RED = 1;
+	int GREEN = 2;
+	int BLUE = 3;
+	int LEOPARD = 4;
+	void changeSkin(int skin);
+}
