822079c89ab5e31d535977ebc5a6d65aa8f06c07
bodii <1401097251@qq.com>
2019-07-25 22:23:44 +0800

    add DVDPlayer tester class

---
+++ java/explain_java/chapter14/player/DVDPlayerTester.java
@@ -0,0 +1,12 @@
+// DVDPlayer02的使用
+
+public class DVDPlayerTester {
+	public static void main(String[] args) {
+		ExPlayer d = new DVDPlayer02();
+
+		d.play();
+		d.slow();
+		d.stop();
+		System.out.println();
+	}
+}
