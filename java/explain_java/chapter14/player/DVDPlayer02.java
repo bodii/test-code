381cbb67a9f5cc44a1832ef24b777e0f538e57d2
bodii <1401097251@qq.com>
2019-07-25 21:30:57 +0800

    add interface extends interface class

---
+++ java/explain_java/chapter14/player/DVDPlayer02.java
@@ -0,0 +1,14 @@
+// -- DVD播放器
+public class DVDPlayer02 implements ExPlayer {
+	public void play() {
+		System.out.println("DVD播放开始!");
+	}
+
+	public void stop() {
+		System.out.println("DVD播放结束!");
+	}
+
+	public void slow() {
+		System.out.println("DVD慢放开始!");
+	}
+}
---
+++ java/explain_java/chapter14/player/ExPlayer.java
@@ -0,0 +1,5 @@
+// 扩展播放器接口（带有慢放功能)
+
+public interface ExPlayer extends Player {
+	void slow();		// 慢放
+}
