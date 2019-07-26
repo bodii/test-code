5709fcb672d0e50687af286ee43e910183f9f74e
bodii <1401097251@qq.com>
2019-07-26 22:07:56 +0800

    char and Unicode

---
+++ java/explain_java/chapter15/CharTester.java
@@ -0,0 +1,13 @@
+// 字符和字符变量
+
+class CharTester {
+	public static void main(String[] args) {
+		char c1 = 50;
+		char c2 = 'A';
+		char c3 = '字';
+
+		System.out.println("c1 = " + c1);
+		System.out.println("c2 = " + c2);
+		System.out.println("c3 = " + c3);
+	}
+}
