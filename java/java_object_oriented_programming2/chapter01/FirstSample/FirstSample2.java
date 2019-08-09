6d2a96903dcf83881336ec74e82994b91e0e7348
bodii <1401097251@qq.com>
2019-08-09 22:15:14 +0800

    add first sample java code test

---
+++ java/java_object_oriented_programming2/chapter01/FirstSample/FirstSample2.java
@@ -0,0 +1,11 @@
+/**
+ * This is the first smaple parogram in core java chapter 3
+ * @version 1.01 1997-03-22
+ * @author Gary Cornell
+ */
+
+public class FirstSample2 {
+	public static void main(String[] args) {
+		System.out.println("We will not use 'Hello, World!'");
+	}
+}
