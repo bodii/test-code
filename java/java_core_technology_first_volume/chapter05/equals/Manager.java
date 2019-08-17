41a67b3d515d7cc81fe972b1a9e8ccbd985527ad
bodii <1401097251@qq.com>
2019-08-17 10:24:25 +0800

    add class equals and hashCode and toString override method

---
+++ java/java_core_technology_first_volume/chapter05/equals/Manager.java
@@ -0,0 +1,37 @@
+package equals;
+
+import equals.Employee;
+
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public boolean equals (Object otherObject) {
+        if (!super.equals(otherObject)) return false;
+
+        Manager other = (Manager) otherObject;
+        return bonus == other.bonus;
+    }
+
+    public int hashCode() {
+        return super.hashCode() + 17 * Double.valueOf(bonus).hashCode();
+    }
+
+    public String toString() {
+        return super.toString() + "[bonus=" + bonus + "]";
+    }
+
+}
