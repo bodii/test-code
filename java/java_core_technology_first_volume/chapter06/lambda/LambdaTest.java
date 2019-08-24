04a1bfffb232234cc216017f6461ee0eceeda1dd
bodii <1401097251@qq.com>
2019-08-24 18:32:35 +0800

    lambda use

---
+++ java/java_core_technology_first_volume/chapter06/lambda/LambdaTest.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+/**
+ * This program demonstrates the use of lambda expressions.
+ * @version 1.1 2019-08
+ * @author wong
+ */
+
+ public class LambdaTest {
+     public static void main(String[] args) {
+         String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+
+         System.out.println(Arrays.toString(planets));
+         System.out.println("Sorted in dictionary order:");
+         Arrays.sort(planets);
+         System.out.println(Arrays.toString(planets));
+         System.out.println("Sorted by length:");
+         Arrays.sort(planets, (first, second) -> first.length() - second.length());
+         System.out.println(Arrays.toString(planets));
+
+         Timer t = new Timer(1000, event -> System.out.println("The time is " + new Date()));
+         t.start();
+
+         JOptionPane.showMessageDialog(null, "Quit program?");
+         System.exit(0);
+     }
+ }
