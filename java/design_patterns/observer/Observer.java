0adad961be780653f2dce04a9749d4a182b3b50e
bodii <1401097251@qq.com>
2019-08-25 09:33:20 +0800

    add observer pattern

---
+++ java/design_patterns/observer/Observer.java
@@ -0,0 +1,5 @@
+package observer;
+
+public interface Observer {
+	public void update(int totalAttendees, int totalSpeakers, String nameEvent);
+}
---
+++ java/design_patterns/observer/Topic.java
@@ -0,0 +1,7 @@
+package observer;
+
+public interface Topic {
+	public void addObserver(Observer observer);
+	public void deleteObserver(Observer observer);
+	public void notifyObservers();
+}
