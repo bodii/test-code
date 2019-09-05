4842aea6b44599c965d910bc4f1745b054b367b6
bodii <1401097251@qq.com>
2019-09-05 22:35:14 +0800

    add generic wildcard use

---
+++ java/java_core_technology_first_volume/chapter08/pair3/Employee.java
@@ -0,0 +1,58 @@
+package pair3;
+
+import java.time.*;
+import java.util.Objects;
+
+public class Employee {
+  private String name;
+  private double salary;
+  private LocalDate hireDay;
+
+  public Employee(String name, double salary, int year, int month, int day) {
+    this.name = name;
+    this.salary = salary;
+    hireDay = LocalDate.of(year, month, day);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public LocalDate getHireDay() {
+    return hireDay;
+  }
+
+  public void raiseSalary(double byPercent) {
+    double raise = salary * byPercent / 100;
+    salary += raise;
+  }
+
+  public boolean equals(Object otherObject) {
+    if (this == otherObject)
+      return true;
+
+    if (otherObject == null)
+      return false;
+
+    if (getClass() != otherObject.getClass())
+      return false;
+
+    Employee other = (Employee) otherObject;
+
+    return Objects.equals(name, other.name) && salary == other.salary 
+    && Objects.equals(hireDay, other.hireDay);
+  }
+
+  public int hashCode() {
+    return Objects.hash(name, salary, hireDay);
+  }
+
+  public String toString() {
+    return getClass().getName() + 
+    String.format("[name=%s, salary=%g, hireDay=%s]",  name, salary, hireDay);
+  }
+}
---
+++ java/java_core_technology_first_volume/chapter08/pair3/PairTest3.java
@@ -0,0 +1,89 @@
+package pair3;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import pair3.Employee;
+import pair3.Manager;
+
+/**
+ * @version  1.1 2019-09
+ * @author wong
+ */
+public class PairTest3 {
+    public static void main(String[] args) {
+        Manager ceo = new Manager("Gus Greedy", 800000, 2003, 12, 15);
+        Manager cfo = new Manager("Sid Sneaky", 600000, 2003, 12, 15);
+        Pair<Manager> buddies = new Pair<>(ceo, cfo);
+        printBuddies(buddies);
+
+        ceo.setBonus(1000000);
+        cfo.setBonus(5000000);
+
+        Manager[] managers = { ceo, cfo };
+
+        Pair<Employee> result = new Pair<>();
+        minmaxBonus(managers, result);
+        System.out.println(
+            "first: " + result.getFirst().getName() + 
+            ", second: " + result.getSecond().getName()
+            );
+
+        maxminBonus(managers, result);
+        System.out.println(
+            "first: " + result.getFirst().getName() + 
+            ", second: " + result.getSecond().getName()
+            );
+    }
+
+    public static void printBuddies(Pair<? extends Employee> p) {
+        Employee first = p.getFirst();
+        Employee second = p.getSecond();
+        System.out.println(first.getName() + " and " + second.getName() + " are buddies.");
+    }
+
+    public static void minmaxBonus(Manager[] a, Pair<? super Manager> result) {
+        if (a.length == 0) return;
+        Manager min = a[0];
+        Manager max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.getBonus() > a[i].getBonus()) min = a[i];
+            if (max.getBonus() < a[i].getBonus()) max = a[i];
+        }
+
+        result.setFirst(min);
+        result.setSecond(max);
+    }
+
+    public static void maxminBonus(Manager[] a, Pair<? super Manager> result) {
+        minmaxBonus(a, result);
+        PairAlg.swapHelper(result);
+    }
+}
+
+class PairAlg {
+    public static boolean hasNulls(Pair<?> p) {
+        return p.getFirst() == null || p.getSecond() == null;
+    }
+
+    public static void swap(Pair<?> p) { swapHelper(p); }
+
+    public static <T> void swapHelper(Pair<T> p) {
+        T t = p.getFirst();
+        p.setFirst(p.getSecond());
+        p.setSecond(t);
+    }
+}
+
+class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() { first = null; second = null; }
+    public Pair(T first, T second) {this.first = first; this.second = second; }
+
+    public T getFirst() { return first; };
+    public T getSecond() { return second; }
+
+    public void setFirst(T newFirst) { first = newFirst; }
+    public void setSecond(T newSecond) { second = newSecond; }
+}
