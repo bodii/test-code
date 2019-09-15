1bf2126127f7940aaeedf4de7bdc8e3a5022e449
bodii <1401097251@qq.com>
2019-09-15 08:54:24 +0800

    add simple frame test

---
+++ java/java_core_technology_first_volume/chapter10/simpleFrame/SimpleFrameTest.java
@@ -0,0 +1,29 @@
+package simpleFrame;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @version 1.1 2019-09
+ * @author wong
+ */
+public class SimpleFrameTest {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(
+			() -> {
+				SimpleFrame frame = new SimpleFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		);
+	}
+}
+
+class SimpleFrame extends JFrame {
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 200;
+
+	public SimpleFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+}
