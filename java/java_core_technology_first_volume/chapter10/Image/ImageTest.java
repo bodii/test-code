1c06adcaaae4a14ea4e437bbf8480494cc296c9d
bodii <1401097251@qq.com>
2019-09-17 23:18:23 +0800

    add image test

---
+++ java/java_core_technology_first_volume/chapter10/Image/ImageTest.java
@@ -0,0 +1,69 @@
+package image;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @version 1.1 2019-09
+ * @author wong
+ */
+public class ImageTest {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(
+			() -> {
+				JFrame frame = new ImageFrame();
+				frame.setTitle("ImageTest");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		);
+	}
+}
+
+
+/**
+ * A frame with an image component
+ */
+class ImageFrame extends JFrame {
+	public ImageFrame() {
+		add(new ImageComponent());
+		pack();
+	}
+}
+
+
+/**
+ * A component that displays a tiled image
+ */
+class ImageComponent extends JComponent {
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 200;
+
+	private Image image;
+
+	public ImageComponent() {
+		image = new ImageIcon("blue-ball.gif").getImage();
+	}
+
+	public void paintComponent(Graphics g) {
+		if (image == null) return;
+
+		int imageWidth = image.getWidth(this);
+		int imageHeight = image.getHeight(this);
+
+		g.drawImage(image, 0, 0, null);
+
+		for (int i = 0; i * imageWidth <= getWidth(); i++)
+			for(int j = 0; j * imageHeight <= getHeight(); j++)
+				if (i + j > 0)
+					g.copyArea(
+							0, 0, 
+							imageWidth, imageHeight, 
+							i * imageWidth, j * imageHeight
+							);
+	}
+
+	public Dimension getPreferredSize() {
+		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+}
