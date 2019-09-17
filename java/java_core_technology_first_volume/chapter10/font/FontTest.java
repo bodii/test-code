7d4c9719b1a35c7db2cfbaded3a10786cbd3744e
bodii <1401097251@qq.com>
2019-09-17 21:48:55 +0800

    add font test

---
+++ java/java_core_technology_first_volume/chapter10/font/FontTest.java
@@ -0,0 +1,72 @@
+package font;
+
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+/**
+ * @version 1.1 2019-09
+ * @author wong
+ */
+public class FontTest {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(
+			() -> {
+				JFrame frame = new FontFrame();
+				frame.setTitle("FontTest");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		);
+	}
+}
+
+/**
+ * A frame with a text message component
+ */
+class FontFrame extends JFrame {
+	public FontFrame() {
+		add(new FontComponent());
+		pack();
+	}
+}
+
+
+/**
+ * A component that shows a centered message in a box.
+ */
+class FontComponent extends JComponent {
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 200;
+
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		String message = "Hello, World!";
+
+		Font f = new Font("Serif", Font.BOLD, 36);
+		g2.setFont(f);
+
+		FontRenderContext context = g2.getFontRenderContext();
+		Rectangle2D bounds = f.getStringBounds(message, context);
+
+		double x = (getWidth() - bounds.getWidth()) / 2;
+		double y = (getHeight() - bounds.getHeight()) / 2;
+
+		double ascent = - bounds.getY();
+		double baseY = y + ascent;
+
+		g2.drawString(message, (int) x, (int) baseY);
+
+		g2.setPaint(Color.LIGHT_GRAY);
+
+		g2.draw(new Line2D.Double(x, baseY, x + bounds.getWidth(), baseY));
+
+		Rectangle2D rect = new Rectangle2D.Double(x, y, bounds.getWidth(), bounds.getHeight());
+		g2.draw(rect);
+	}
+
+	public Dimension getPreferredSize() {
+		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+}
---
+++ java/java_core_technology_first_volume/chapter10/font/ListFonts.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class ListFonts {
+	public static void main(String[] args) {
+		String[] fontNames = GraphicsEnvironment
+			.getLocalGraphicsEnvironment()
+			.getAvailableFontFamilyNames();
+		
+		for (String fontName : fontNames)
+			System.out.println(fontName);
+	}
+}
