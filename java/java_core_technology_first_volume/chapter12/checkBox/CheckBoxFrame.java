d0554c2e2f4c70e557891638407cb397317b8a24
bodii <1401097251@qq.com>
2019-09-24 20:09:40 +0800

    add check box

---
+++ java/java_core_technology_first_volume/chapter12/checkBox/CheckBoxFrame.java
@@ -0,0 +1,43 @@
+package checkBox;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * A frame with a sample text label and check boxes for selecting font
+ * attributes.
+ */
+public class CheckBoxFrame extends JFrame {
+	private JLabel label;
+	private JCheckBox bold;
+	private JCheckBox italic;
+	private static final int FONTSIZE = 24;
+
+	public CheckBoxFrame() {
+		label = new JLabel("The quick brown fox jumps over the lazy dog.");
+		label.setFont(new Font("Serif", Font.BOLD, FONTSIZE));
+		add(label, BorderLayout.CENTER);
+
+		ActionListener listener = event -> {
+			int mode = 0;
+			if (bold.isSelected()) mode += Font.BOLD;
+			if (italic.isSelected()) mode += Font.ITALIC;
+			label.setFont(new Font("Serif", mode, FONTSIZE));
+		};
+
+
+		JPanel buttonPanel = new JPanel();
+		bold = new JCheckBox("Bold");
+		bold.addActionListener(listener);
+		bold.setSelected(true);
+		buttonPanel.add(bold);
+
+		italic = new JCheckBox("Italic");
+		italic.addActionListener(listener);
+		buttonPanel.add(italic);
+
+		add(buttonPanel, BorderLayout.SOUTH);
+		pack();
+	}
+}
---
+++ java/java_core_technology_first_volume/chapter12/checkBox/CheckBoxTest.java
@@ -0,0 +1,15 @@
+package checkBox;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import checkBox.CheckBoxFrame;
+
+public class CheckBoxTest{
+	public static void main(String[] args) {
+		JFrame frame = new CheckBoxFrame();
+		frame.setTitle("check box test");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
