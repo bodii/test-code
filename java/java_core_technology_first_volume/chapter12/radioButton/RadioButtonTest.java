0945ea99d9dfe1fbec781b4f16e1a170603de991
bodii <1401097251@qq.com>
2019-09-24 21:17:39 +0800

    add radio button test

---
+++ java/java_core_technology_first_volume/chapter12/radioButton/RadioButtonTest.java
@@ -0,0 +1,15 @@
+package radioButton;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import radioButton.RadioButtonFrame;
+
+public class RadioButtonTest {
+    public static void main(String[] args) {
+        JFrame frame = new RadioButtonFrame();
+        frame.setTitle("radio button test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
