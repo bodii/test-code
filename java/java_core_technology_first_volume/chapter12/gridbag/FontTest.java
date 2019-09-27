0bace3947ea430a9e9573f30cae9b882ac0f88fb
bodii <1401097251@qq.com>
2019-09-27 23:02:29 +0800

    add gridbag test

---
+++ java/java_core_technology_first_volume/chapter12/gridbag/FontTest.java
@@ -0,0 +1,15 @@
+package gridbag;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import gridbag.FontFrame;
+
+public class FontTest {
+    public static void main(String[] args) {
+        JFrame frame = new FontFrame();
+        frame.setTitle("gridbag font test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ java/java_core_technology_first_volume/chapter12/gridbag/GBC.java
@@ -0,0 +1,60 @@
+package gridbag;
+
+import java.awt.*;
+
+/**
+ * This class simplefiles the use of the GridBagConstraints class.
+ * @version 1.1 2019-09
+ * @author wong
+ */
+public class GBC extends GridBagConstraints {
+    /**
+     * Constructs a GBC with a given gridx and gridy position and all other grid
+     * bag constraint values set to the default.
+     * @param gridx the gridx position
+     * @param gridy the gridy position
+     */
+    public GBC(int gridx, int gridy) {
+        this.gridx = gridx;
+        this.gridy = gridy;
+    }
+
+    public GBC(int gridx, int gridy, int gridwidth, int gridheight) {
+        this.gridx =gridx;
+        this.gridy = gridy;
+        this.gridwidth = gridwidth;
+        this.gridheight = gridheight;
+    }
+
+    public GBC setAnchor(int anchor) {
+        this.anchor = anchor;
+        return this;
+    }
+
+    public GBC setFill(int fill) {
+        this.fill = fill;
+        return this;
+    }
+
+    public GBC setWeight(double weightx, double weighty) {
+        this.weightx = weightx;
+        this.weighty = weighty;
+        return this;
+    }
+
+    public GBC setInsets(int distance) {
+        this.insets = new Insets(distance, distance, distance, distance);
+        return this;
+    }
+
+    public GBC setInsets(int top, int left, int bottom, int right) {
+        this.insets = new Insets(top, left, bottom, right);
+        return this;
+    }
+
+    public GBC setIpad(int ipadx, int ipady) {
+        this.ipadx = ipadx;
+        this.ipady = ipady;
+        return this;
+    }
+}
