cfb41af1e7ee7c2af3bcccc638b46cc477c4ba30
bodii <1401097251@qq.com>
2019-09-28 19:14:10 +0800

    add circle layout test

---
+++ java/java_core_technology_first_volume/chapter12/circleLayout/CircleLayoutTest.java
@@ -0,0 +1,15 @@
+package circleLayout;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import circleLayout.CircleLayoutFrame;
+
+public class CircleLayoutTest {
+    public static void main(String[] args) {
+        JFrame frame = new CircleLayoutFrame();
+        frame.setTitle("circle layout test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+} 
