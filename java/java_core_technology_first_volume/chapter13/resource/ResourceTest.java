5a43813a0ad813affad572a66594b907141e5bee
bodii <1401097251@qq.com>
2019-10-02 12:04:56 +0800

    add jar program exceutes the read resource

---
+++ java/java_core_technology_first_volume/chapter13/resource/ResourceTest.java
@@ -0,0 +1,49 @@
+package resource;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.*;
+
+
+/**
+ * @version 1.0 2019-10
+ * @author wong
+ */
+public class ResourceTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(
+            () -> {
+                JFrame frame = new ResourceTestFrame();
+                frame.setTitle("ResourceTest");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        );
+    }
+}
+
+/**
+ * A frame that loads and text resources.
+ */
+class ResourceTestFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public ResourceTestFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        URL aboutURL = getClass().getResource("about.gif");
+        Image img = new ImageIcon(aboutURL).getImage();
+        setIconImage(img);
+
+        JTextArea textArea = new JTextArea();
+        InputStream stream = getClass().getResourceAsStream("about.txt");
+        try (Scanner in = new Scanner(stream, "UTF-8")) {
+            while(in.hasNext())
+                textArea.append(in.nextLine() + "\n");
+        }
+        add (textArea);
+    }
+}
