e5c70a00e47a87d2b6fca9d53513a5af38f3d089
bodii <1401097251@qq.com>
2019-10-05 13:00:58 +0800

    add multithreaded test

---
+++ java/java_core_technology_first_volume/chapter14/bounceThread/BallComponent.java
@@ -0,0 +1,34 @@
+package bounceThread;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import bounceThread.Ball;
+
+/**
+ * The component that draws the balls.
+ * @version 1.1 2019-10
+ * @author wong
+ */
+public class BallComponent extends JPanel {
+    private static final int DEFAULT_WIDTH = 450;
+    private static final int DEFAULT_HEIGHT = 350;
+
+    private java.util.List<Ball> balls = new ArrayList<>();
+
+    public void add(Ball b) {
+        balls.add(b);
+    }
+
+    public void printComponent(Graphics g) {
+        super.printComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for (Ball b : balls) {
+            g2.fill(b.getShape());
+        }
+    }
+
+    public Dimension getPerferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
