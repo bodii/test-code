b279e72941fbd3a85c46402861f2dc9b58e8fb0f
bodii <1401097251@qq.com>
2019-10-18 18:02:00 +0800

    add minesweeper game

--- shell/advanced_bash_skills/minesweeper.sh
+++ shell/advanced_bash_skills/minesweeper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # ==================================
 #                               扫雷游戏
@@ -26,9 +26,35 @@ d="-1 0 1 -2 -3"
 e="1 2 20 21 10 0 -10 -20 -23 -2 -1"
 f="1 2 3 35 30 30 22 10 0 -10 -20 -25 -30 -35 -3 -2 -1"
 g="1 4 6 9 10 15 20 30 -30 -24 -11 -10 -9 -8 -7"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m" # no color
 
 # 声明
 declare -a room # 声明一个room数组，他用来表示雷区的每一格
+
+function usage() {
+    cat <<_EOF_
+    This game is made for fun, it has simple rules.
+    * Do not cheat.
+    * You can re-select an element if only it's - empty.
+    * if even, double digits are given, only the
+        first will be considered.
+    * When the game is over, it is over.
+
+    Shown down here is a metrics 10x10 in size and
+    to play you have to enter the coordinates.
+
+    NOTE: To choose col- g, row- 5, give input - g5
+
+_EOF_
+}
+
+function time_to_quit() {
+    printf '\n\n%s\n\n' "info: Sadly! You opted to quit!!"
+    exit 1
+} 
+
 
 function plough() {
     r=0
@@ -68,29 +94,44 @@ function get_free_fields () {
     done
 }
 
-plough
+# plough
 
 
 # --------------------------------------------------------
 # 创建玩家逻辑
 # --------------------------------------------------------
-colm=${opt:0:1}     # 得到第一个字符，一个字母
-ro=${opt:1:1}           # 得到第二个字符，一个整数
-case $colm in 
-    a ) o=1;;
-    b ) o=2;;
-    c ) o=3;;
-    d ) o=4;;
-    e ) o=5;;
-    f  ) o=6;;
-    g ) o=7;;
-    h ) o=8;;
-    i  ) o = 9;;
-    j  ) o=10;;
-esac
+function get_coordinates() {
+    colm=${opt:0:1}     # 得到第一个字符，一个字母
+    ro=${opt:1:1}           # 得到第二个字符，一个整数
+    case $colm in 
+        a ) o=1;;
+        b ) o=2;;
+        c ) o=3;;
+        d ) o=4;;
+        e ) o=5;;
+        f  ) o=6;;
+        g ) o=7;;
+        h ) o=8;;
+        i  ) o = 9;;
+        j  ) o=10;;
+    esac
 
-i=$(((ro*10)+o))    # 遵循运算规则，算出最终值
-is_null_field $i $(shuf -i 0-5 -n 1)    # 调用自定义函数，判断其指向空/可选择单元格
+    i=$(((ro*10)+o))    # 遵循运算规则，算出最终值
+    is_null_field $i $(shuf -i 0-5 -n 1)    # 调用自定义函数，判断其指向空/可选择单元格
+    if [[ $not_allowed -eq 1 ]] || [[ ! "$colm" =~ [a-j] ]]
+    then
+        printf "$RED \n%s: %s \n$NC" "warning" "not allowed!!!!!"
+    else
+        get_mines
+        plough
+        get_free_fields
+        if [[ "$m" = "X" ]]
+        then
+        elif
+        then
+        fi
+    fi
+}
 
 # --------------------------------------------------------
 # 3. 创建判断单元格是否可选的逻辑
