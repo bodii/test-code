7e0dfce13a83a9ac0bae9d634b0e2d39afdaad03
bodii <1401097251@qq.com>
2019-11-04 22:22:43 +0800

    add python algorithn test is bubble and select sort

---
+++ python/algorithm/selectSort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# PW @ 2019-11-04 21:27:50
+
+
+def select_sort(alist: list):
+    """
+    选择排序
+    """
+    llen = len(alist)
+    for i in range(llen - 1):
+        min_index = i
+        for j in range(i+1, llen):
+            if alist[j] < alist[min_index]:
+                min_index = j
+
+        if min_index != i:
+            alist[i], alist[min_index] = alist[min_index], alist[i]
+
+        
+
+alist = [7, 5, 3, 6, 44, 22, 99, 11]
+select_sort(alist)
+
+print(alist)
