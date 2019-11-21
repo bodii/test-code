37e9b920ebe5dc2659f01a4cf87e5e927be8a1a0
bodii <1401097251@qq.com>
2019-11-21 17:31:49 +0800

    add embedded static class test

---
+++ java/effective_java/chapter01/Adult.java
@@ -0,0 +1,7 @@
+public class Adult {
+    public static void main(String[] args) {
+        try (Room myRoom = new Room(7)) {
+            System.out.println("Goodbye");
+        }
+    }
+}
---
+++ java/effective_java/chapter01/Teenager.java
@@ -0,0 +1,6 @@
+public class Teenager {
+    public static void main(String[] args) {
+        new Room(99);
+        System.out.println("Peace out");
+    }
+}
