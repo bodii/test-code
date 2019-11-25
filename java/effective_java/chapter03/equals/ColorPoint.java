7a70af7f6845572a30a6f8021fcf6512149bfe58
bodii <1401097251@qq.com>
2019-11-25 21:54:25 +0800

    add equals test

---
+++ java/effective_java/chapter03/equals/ColorPoint.java
@@ -0,0 +1,16 @@
+package equals;
+
+public class ColorPoint extends Point {
+	private final Color color;
+
+	public ColorPoint(int x, int y, Color color) {
+		super(x, y);
+		this.color = color;
+	}
+
+	@Override public boolean equals(Object o) {
+		if (!(o instanceof ColorPoint))
+			return false;
+		return super.equals(o) && ((ColorPoint) o).color == color;
+	}
+}
