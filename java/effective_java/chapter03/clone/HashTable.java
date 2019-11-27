fc39abddaf3ab1b30d6599553546eb866f8cc179
bodii <1401097251@qq.com>
2019-11-27 21:56:31 +0800

    add clone

---
+++ java/effective_java/chapter03/clone/HashTable.java
@@ -0,0 +1,33 @@
+package clone;
+
+public class HashTable implements Cloneable {
+    private Entry[] buckets = ... ;
+    private static class Entry {
+        final Object key;
+        Object value;
+        Entry next;
+
+        Entry(Object key, Object value, Entry next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+        Entry deepCopy() {
+            return new Entry(key, value, next == null ? null : next.deepCopy());
+        }
+    }
+
+    // Broken clone method - results in shared mutable state!
+    @Override public HashTable clone() {
+        try {
+            HashTable result = (HashTable) super.clone();
+            result.buckets = new Entry[buckets.length];
+            for (int i = 0; i < buckets.length; i++) 
+                if (buckets[i] != null)
+                    result.buckets[i] = buckets[i].deepCopy();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
