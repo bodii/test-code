3f4f445b05a525a5001f80103300182a484f7124
bodii <1401097251@qq.com>
2019-12-29 15:35:40 +0800

    add Set class override function

---
+++ java/effective_java/chapter04/InstrumentedHashSet.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+// Rroken - Inappropriate use of inheritance!
+public class InstrumentedHashSet<E> extends HashSet<E> {
+    // The number of attempted element insertions
+    private int addCount = 0;
+
+    public InstrumentedHashSet() {
+
+    }
+
+    public InstrumentedHashSet(int initCap, float loadFactor) {
+        super(initCap, loadFactor);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.szie();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+}
---
+++ java/effective_java/chapter04/InstrumentedSet.java
@@ -0,0 +1,25 @@
+import java.util.Set;
+import java.util.Collection;
+
+// Wrapper class - uses composition in place of inheritance
+public class InstrumentedSet<E> extends ForwardingSet<E> {
+    private int addCount = 0;
+
+    public InstrumentedSet(Set<E> s) {
+        super(s);
+    }
+
+    @Override public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+}
