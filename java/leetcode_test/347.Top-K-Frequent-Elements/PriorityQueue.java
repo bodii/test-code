108695e6e5e8fda7d47bf6bd9e26d254d54b77cd
bodii <1401097251@qq.com>
2020-02-06 11:58:19 +0800

    add top k frequent elements Solution class and test

---
+++ java/leetcode_test/347.Top-K-Frequent-Elements/PriorityQueue.java
@@ -0,0 +1,68 @@
+package top_k_frequent_elements;
+
+import top_k_frequent_elements.Queueable;
+import top_k_frequent_elements.MaxHeap;
+/**
+ * 优先队列类
+ * @param <E>
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queueable<E> {
+    private MaxHeap<E> heap;  // 最大二叉堆的优先队列对象
+
+    /**
+     * 构造函数，用于实例化一个最大二叉堆对象
+     */
+    public PriorityQueue() {
+        heap = new MaxHeap<>();
+    }
+
+    /**
+     * 获取优先队列的元素个数
+     * 
+     * @return 返回元素个数
+     */
+    @Override
+    public int getSize() {
+        return heap.size();
+    }
+
+    /**
+     * 检测优先队列是否为空
+     * 
+     * @return 是否为空
+     */
+    @Override 
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+
+    /**
+     * 向优先队列中添加元素
+     * 
+     * @param e 要添加的元素
+     */
+    @Override
+    public void enqueue(E e) {
+        heap.add(e);
+    }
+
+    /**
+     * 查看优先队列的头元素，也就是最大元素
+     * 
+     * @return 头部元素
+     */
+    @Override
+    public E getFront() {
+        return heap.findMax();
+    }
+
+    /**
+     * 从优先队列中取出头元素
+     * 
+     * @return 头元素
+     */
+    @Override
+    public E dequeue() {
+        return heap.extractMax();
+    }
+}
---
+++ java/leetcode_test/347.Top-K-Frequent-Elements/Solution2.java
@@ -0,0 +1,75 @@
+package top_k_frequent_elements;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.PriorityQueue;
+
+class Solution2 {
+    /**
+     * 装载一个元素和它的频率的类
+     * 
+     * @param <E>
+     */
+    private class Frequency implements Comparable<Frequency> {
+        public int e; // 元素
+        public int freq; // 出现的频率
+
+        public Frequency(int e, int freq ) {
+            this.e = e;
+            this.freq = freq;
+        }
+
+        /**
+         * 当前元素的频率和另一个计算频率类比较函数
+         * 
+         * @param another 另一个计算频率类
+         * @return 比较大小的结果
+         */
+        @Override 
+        public int compareTo(Frequency another) {
+            if (freq < another.freq) // 当前频率小于另一个类元素的频率时
+                return -1;
+            else if (freq > another.freq)
+                return 1;
+            return 0;
+        }
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        
+        for (int num : nums) {
+            if (map.containsKey(num)) 
+                map.put(num, map.get(num) + 1);
+            else 
+                map.put(num, 1);
+        }
+
+        PriorityQueue<Frequency> queue = new PriorityQueue<>();
+        for (int m : map.keySet()) {
+            if (queue.size() < k)
+                queue.add(new Frequency(m, map.get(m)));
+            else if (map.get(m) > queue.peek().freq) {
+                queue.remove();
+                queue.add(new Frequency(m, map.get(m)));
+            }
+        }
+
+        LinkedList<Integer> list = new LinkedList<>();
+        while(!queue.isEmpty())
+            list.add(queue.remove().e);
+
+        return list;
+    }
+}
+
+class Test2 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,1,2,2,3};
+        int k = 2;
+
+        Solution2 solution = new Solution2();
+        System.out.println(solution.topKFrequent(nums, k));
+    }
+}
