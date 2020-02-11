f566ab76ec3ddb6f6de38546a50447849351b9e7
bodii <1401097251@qq.com>
2020-02-11 14:56:01 +0800

    add roman to integer Solution class and test

--- java/leetcode_test/0013.Roman-to-Integer/Solution.java
+++ java/leetcode_test/0013.Roman-to-Integer/Solution.java
@@ -1,73 +1,61 @@
 package roman_to_integer;
 
-import java.util.ArrayList;
-
 class Solution {
     public int romanToInt(String s) {
-        if (s.length() == 0 ) 
+        if (s.length() == 0)
             throw new IllegalArgumentException("The string is not empty!");
 
-        ArrayList<Integer> arr = new ArrayList<>(s.length());
+        int result = 0;
+        for (int i = 0; i < s.length() -1; i ++) {
+            if (getRomanToInt(s.charAt(i)) >= getRomanToInt(s.charAt(i+1)))
+                result += getRomanToInt(s.charAt(i));
+            else
+                result -=  getRomanToInt(s.charAt(i));
+        }
+        result += getRomanToInt(s.charAt(s.length()-1));
 
-        for (int i = s.length() -1; i >= 0; i --) {
-            if ( 'I' == s.charAt(i)) {
-                int num = 1, index = --i;
-                while('I' == s.charAt(index--)) {
-                    i --;
-                    num ++;
-                }
-                if ('V' == s.charAt(i)) {
-                    i --;
-                    num += 5;
-                }
-                arr.add(num);
-            } else if ('V' == s.charAt(i)) {
-                int num = 5;
-                if ('I' == s.charAt(--i)) {
-                    num --;
-                    i --;
-                }
-                arr.add(num);
-            } else if ('X' == s.charAt(i)) {
-                int num = 1;
-                if ('I' == s.charAt(--i)) {
-                    num = 9;
-                    i --;
-                }
-                arr.add(num);
-            } else if 
+        return result;
+    }
+
+    private int getRomanToInt(char c) {
+        switch (c) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
         }
     }
 }
-
 
 class Test {
     public static void main(String[] args) {
         String s1 = "III";
         Solution solution = new Solution();
-        System.out.println(
-            solution.romanToInt(s1)
-        );
+        System.out.println(solution.romanToInt(s1));
 
         String s2 = "IV";
-        System.out.println(
-            solution.romanToInt(s2)
-        );
+        System.out.println(solution.romanToInt(s2));
 
         String s3 = "IX";
-        System.out.println(
-            solution.romanToInt(s3)
-        );
+        System.out.println(solution.romanToInt(s3));
 
         String s4 = "LVIII";
-        System.out.println(
-            solution.romanToInt(s4)
-        );
+        System.out.println(solution.romanToInt(s4));
 
         String s5 = "MCMXCIV";
-        System.out.println(
-            solution.romanToInt(s5)
-        );
+        System.out.println(solution.romanToInt(s5));
 
     }
 }
