cf698e25fee1c60e21ee1e052aa6309c07aacef2
wangchangjiang <1401097251@qq.com>
2020-02-21 11:06:48 +0800

    add plus on Solution class and test

--- java/leetcode_test/0066.plus_one/Solution.java
+++ java/leetcode_test/0066.plus_one/Solution.java
@@ -2,9 +2,22 @@ package plus_one;
 
 class Solution {
     public int[] plusOne(int[] digits) {
-        int len = digits.length;
-        if (digits[len - 1] < 9 ) return digits;
+        digits[digits.length -1] ++;
+        if (digits[digits.length -1] != 10)
+            return digits;
 
+        for (int i = digits.length - 1; i >= 0; i --) {
+            if (digits[i] == 10) {
+                digits[i] = 0;
+                if (i > 0)
+                    digits[i -1] ++;
+            } else 
+                return digits;
+
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
 
         return digits;
     }
@@ -22,6 +35,33 @@ class Test {
         for (int i = 0; i < len; i++) {
             System.out.print(newDigits[i]);
             if (i != len - 1) 
+                System.out.print(", ");
+        }
+        System.out.println();
+
+
+        int[] digits2 = new int[]{4,3,2,1};
+
+        int[] newDigits2 = s.plusOne(digits2);
+        System.out.println("加一后的数组值是：");
+        int len2 = newDigits2.length;
+        for (int i = 0; i < len2; i++) {
+            System.out.print(newDigits2[i]);
+            if (i != len2 - 1) 
+                System.out.print(", ");
+        }
+
+        System.out.println();
+
+
+        int[] digits3 = new int[]{9,8,9};
+
+        int[] newDigits3 = s.plusOne(digits3);
+        System.out.println("加一后的数组值是：");
+        int len3 = newDigits3.length;
+        for (int i = 0; i < len3; i++) {
+            System.out.print(newDigits3[i]);
+            if (i != len3 - 1) 
                 System.out.print(", ");
         }
 
