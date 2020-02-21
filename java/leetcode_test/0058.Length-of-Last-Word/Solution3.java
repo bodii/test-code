ea4afb86363171be666d48e937eafe1eaecbf49e
wangchangjiang <1401097251@qq.com>
2020-02-21 15:42:50 +0800

    add length of last word Ideas for solving this problem

--- java/leetcode_test/0058.Length-of-Last-Word/Solution3.java
+++ java/leetcode_test/0058.Length-of-Last-Word/Solution3.java
@@ -1,5 +1,15 @@
 package length_of_last_word;
 
+/*
+
+解题思路
+
+从右向左遍历，并声明一个返回值(result)为0数，用来记录最后一个单词的长度。
+返回值(result)是0时，当前字符是 ’ ’ 时，说明还没有触探到最后一个单词的边界，跳过此次遍历机会；
+当返回值(result)非0时，如果当前字符是’ ’ 时，说明最后一个单词的长度已经获取完毕结束遍历，否则返回值(result)+1；
+最终返回result。
+
+ */
 class Solution3 {
     public int lengthOfLastWord(String s) {
         int right = s.length();
