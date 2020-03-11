d4fee7f1b42c4349ecec8ecbba48b30dd5078b6b
wangchangjiang <1401097251@qq.com>
2020-03-11 16:56:48 +0800

    static import

---
+++ java/effective_java/chapter14/PhysicalConstants.java
@@ -0,0 +1,9 @@
+package chapter14;
+
+public class PhysicalConstants {
+    private PhysicalConstants() {}
+
+    public static final double AVOGAOROS_NUMBER = 6.022_140_857e23;
+    public static final double BOLTZMANN_CONST  = 1.380_648_52e-23;
+    public static final double ELECTRON_MASS    = 9.109_383_56e-31;
+}
---
+++ java/effective_java/chapter14/TestPhysicalConstants.java
@@ -0,0 +1,9 @@
+package chapter14;
+
+import static chapter14.PhysicalConstants.*;
+
+public class TestPhysicalConstants {
+    double atoms(double mols) {
+        return AVOGAOROS_NUMBER * mols;
+    }
+}
