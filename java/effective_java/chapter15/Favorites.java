45611e74eb658852726f1bfb050125d31bccbaa4
wangchangjiang <1401097251@qq.com>
2020-03-24 17:07:48 +0800

    add typesafe heterogeneous container

---
+++ java/effective_java/chapter15/Favorites.java
@@ -0,0 +1,31 @@
+package chapter15;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.HashMap;
+
+public class Favorites {
+    private Map<Class<?>, Object> favorites = new HashMap<>();
+
+    public <T> void putFavorite(Class<T> type, T instance) {
+        favorites.put(Objects.requireNonNull(type), instance);
+    }
+
+    public <T> T getFavorite(Class<T> type) {
+        return type.cast(favorites.get(type));
+    }
+
+    public static void main(String[] args) {
+        Favorites f = new Favorites();
+        f.putFavorite(String.class, "Java");
+        f.putFavorite(Integer.class, 0xcafebaba);
+        f.putFavorite(Class.class, Favorites.class);
+
+        String favoriteString = f.getFavorite(String.class);
+        int favoriteInteger = f.getFavorite(Integer.class);
+        Class<?> favoriteClass = f.getFavorite(Class.class);
+        System.out.printf("%s %x %s%n", favoriteString,
+            favoriteInteger, favoriteClass.getName()
+            );
+    }
+}
