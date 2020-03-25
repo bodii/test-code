c14c54a8a6a9fde4bbc83f60347be865332810c5
wangchangjiang <1401097251@qq.com>
2020-03-25 15:07:43 +0800

    generics class

---
+++ java/effective_java/chapter15/PayrollDay.java
@@ -0,0 +1,25 @@
+package chapter15;
+
+public enum PayrollDay {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY,
+    SATURDAY, SUNDAY;
+
+    private static final int MINS_PER_SHIFT = 8 * 60;
+
+    int pay(int minutesWorked, int payRate) {
+        int basePay = minutesWorked * payRate;
+
+        int overtimePay;
+
+        switch (this) {
+            case SATURDAY: case SUNDAY:
+                overtimePay = basePay / 2;
+                break;
+            default: 
+                overtimePay = minutesWorked <= MINS_PER_SHIFT ? 0 :
+                    (minutesWorked - MINS_PER_SHIFT) * payRate / 2;
+        }
+
+        return basePay + overtimePay;
+    }
+}
