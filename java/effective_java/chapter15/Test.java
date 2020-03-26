f4e15caf12b015aff13a4a48c9207466f9591f03
wangchangjiang <1401097251@qq.com>
2020-03-26 17:43:58 +0800

    add annotation processor

---
+++ java/effective_java/chapter15/Test.java
@@ -0,0 +1,13 @@
+package chapter15;
+
+import java.lang.annotation.*;
+
+/**
+ * Indicates that the annotated method is a test method.
+ * Use only on parameterless static methods.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+
+}
