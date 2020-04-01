198dd7adbe43ec777d6157d9044446725b803e5e
bodii <1401097251@qq.com>
2020-04-01 16:21:45 +0800

    add rust guessing game

--- rust/the_rust_programming_language_book/guessing_name/src/main.rs
+++ rust/the_rust_programming_language_book/guessing_name/src/main.rs
@@ -1,13 +1,41 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        println!("You guessed: {}", guess);
+
+        // let x = 5;
+        // let y = 10;
+
+        // println!("x = {} and y = {}", x, y);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
