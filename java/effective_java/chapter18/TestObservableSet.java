0d5d92d0b3da75c0375552d7f20dd8f0254effc4
bodii <1401097251@qq.com>
2020-05-14 18:01:09 +0800

    add test file

--- java/effective_java/chapter18/TestObservableSet.java
+++ java/effective_java/chapter18/TestObservableSet.java
@@ -1,7 +1,12 @@
 package chapter18;
 
 import java.util.HashSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import chapter18.ObservableSet;
+import chapter18.SetObserver;
 
 public class TestObservableSet {
     public static void main(String[] args) {
@@ -9,6 +14,23 @@ public class TestObservableSet {
             new ObservableSet<>(new HashSet<>());
 
         set.addObserver((s, e) -> System.out.println(e));
+
+        set.addObserver(new SetObserver<>() {
+            public void added(ObservableSet<Integer> s, Integer e) {
+                System.out.println(e);
+                if (e == 23) {
+                    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+                    try {
+                        exec.submit(() -> s.removeObserver(this)).get();
+                    } catch (ExecutionException | InterruptedException ex) {
+                        throw new AssertionError(ex);
+                    } finally {
+                        exec.shutdown();
+                    }
+                }
+            }
+        });
 
         for (int i = 0; i < 100; i ++)
             set.add(i);
