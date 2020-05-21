7feedf8182504a74b324f7bcfbba44a9e25dc4e1
bodii <1401097251@qq.com>
2020-05-21 16:14:17 +0800

    add serialized object use case

---
+++ java/effective_java/chapter18/TestMutablePeriod.java
@@ -0,0 +1,17 @@
+package chapter18;
+
+import java.util.Date;
+
+public class TestMutablePeriod {
+    public static void main(String[] args) {
+        MutablePeriod mp = new MutablePeriod();
+        Period p = mp.period;
+        Date pEnd = mp.end;
+
+        pEnd.setYear(78);
+        System.out.println(p);
+
+        pEnd.setYear(69);
+        System.out.println(p);
+    }
+}
