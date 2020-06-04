e9fb150335417fe5340e7b6449dca1cfb7b62d22
bodii <1401097251@qq.com>
2020-06-04 10:10:56 +0800

    immutable class use test

---
+++ java/graphical_java_multi_threaded_design_pattern/chapter02/PersonMain.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person alice = new Person("Alice", "Alaska");
+        new PrintPersonThread(alice).start();
+        new PrintPersonThread(alice).start();
+        new PrintPersonThread(alice).start();
+    }
+}
---
+++ java/graphical_java_multi_threaded_design_pattern/chapter02/PrintPersonThread.java
@@ -0,0 +1,15 @@
+package chapter02;
+
+public class PrintPersonThread extends Thread {
+    private  Person person;
+    public PrintPersonThread(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            System.out.println(Thread.currentThread().getName() + " prints " + person);
+        }
+    }
+}
