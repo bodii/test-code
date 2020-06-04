5f0889a759090f430089616f2c8a63aaf8909fe8
bodii <1401097251@qq.com>
2020-06-04 18:11:04 +0800

    add reqeust thread test example

---
+++ java/graphical_java_multi_threaded_design_pattern/chapter03/ServerThread.java
@@ -0,0 +1,28 @@
+package chapter03;
+
+import java.util.Random;
+
+public class ServerThread extends Thread {
+    private final Random random;
+    private final RequestQueue requestQueue;
+
+    public ServerThread(RequestQueue requestQueue, String name, long seed) {
+        super(name);
+        this.requestQueue = requestQueue;
+        this.random = new Random(seed);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i ++) {
+            try {
+                Request request = requestQueue.getRequest();
+                System.out.println(Thread.currentThread().getName() + " handles " + request);
+
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                
+            }
+        }
+    }
+}
