62a345f3653d68bc4f0e18fc421af9fa65d21f52
bodii <1401097251@qq.com>
2020-06-21 11:49:32 +0800

    add ScheduledExecutorService interface class use

---
+++ java/graphical_java_multi_threaded_design_pattern/chapter07/Host7.java
@@ -0,0 +1,32 @@
+package chapter07;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Host7 {
+    private final Helper helper = new Helper();
+
+    private final ScheduledExecutorService scheduledExecutorService;
+
+    public Host7(ScheduledExecutorService scheduledExecutorService) {
+        this.scheduledExecutorService = scheduledExecutorService;
+    }
+
+    public void request(final int count, final char c) {
+        System.out.println("    request(" + count + ", " + c + ") BEGIN");
+
+        scheduledExecutorService.schedule(
+            new Runnable(){
+            
+                @Override
+                public void run() {
+                   helper.handle(count, c);
+                }
+            },
+            3L, 
+            TimeUnit.SECONDS
+            );
+
+        System.out.println("    request(" + count + ", " + c + ") END");
+    }
+}
