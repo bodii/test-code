af090b2d70affa2e472307ef623a979015cdf37f
bodii <1401097251@qq.com>
2020-07-05 09:44:22 +0800

    add not use thread specific storage pattern

---
+++ java/graphical_java_multi_threaded_design_pattern/chapter11/LogMain.java
@@ -0,0 +1,19 @@
+package chapter11;
+
+public class LogMain {
+    public static void main(String[] args) {
+        System.out.println("BEGIN");
+
+        for (int i = 0; i < 10; i++) {
+            Log.println("main: i = " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                
+            }
+        }
+        Log.close();
+
+        System.out.println("END");
+    }
+}
