0e6cd2fa0541c9713fee601e5b8ad1f31951d23c
bodii <1401097251@qq.com>
2020-08-06 22:30:37 +0800

    add apply function

---
+++ kotlin/kotlin_from_entry_to_advanced_combat/src/com/easykotlin/test/chapter5/ApplyFunciton.kt
@@ -0,0 +1,27 @@
+package com.easykotlin.test.chapter5
+
+fun testApply() {
+    // 普通写法
+    val list = mutableListOf<String>()
+    list.add("A")
+    list.add("B")
+    list.add("C")
+    println("普通写法 list = $list")
+    println(list)
+
+    // 使用apple()函数的写法
+    val a = ArrayList<String>().apply {
+        add("A")
+        add("B")
+        add("C")
+        println("使用apply函数写法this=$this")
+    }
+
+    println(a)
+    // 等价于
+    a.let { println(it) }
+}
+
+fun main(args: Array<String>) {
+    testApply()
+}
