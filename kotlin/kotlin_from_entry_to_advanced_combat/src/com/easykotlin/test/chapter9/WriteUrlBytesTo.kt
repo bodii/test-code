3f60fec2afe66a1664c11dd809a9898639674f09
bodii <1401097251@qq.com>
2020-08-10 12:10:09 +0800

    add request url image and save

--- kotlin/kotlin_from_entry_to_advanced_combat/src/com/easykotlin/test/chapter9/WriteUrlBytesTo.kt
+++ kotlin/kotlin_from_entry_to_advanced_combat/src/com/easykotlin/test/chapter9/WriteUrlBytesTo.kt
@@ -12,4 +12,6 @@ fun main(args: Array<String>) {
     val path = "src/com/easykotlin/test/chapter9/"
     val url = "http://cn.bing.com"
     writeUrlBytesTo(path + "bing.txt", url)
+
+    writeUrlBytesTo(path + "OIP.dCFZwNZRvKc89JHiYNC4fwHaE8.jpg", "https://tse2-mm.cn.bing.net/th/id/OIP.dCFZwNZRvKc89JHiYNC4fwHaE8?pid=Api&rs=1")
 }
