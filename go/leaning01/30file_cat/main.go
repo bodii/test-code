f885c197688ccdacb1b78975a7d3a0e559dfdeb7
bodii <1401097251@qq.com>
2020-09-22 23:56:04 +0800

    add read input for show to console func

---
+++ go/leaning01/30file_cat/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"io"
+	"flag"
+)
+
+
+func cat(r *bufio.Reader) {
+	for {
+		buf, err := r.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Fprintf(os.Stdout, "%s", buf)
+	}
+}
+
+func main() {
+	// 解析命令行参数
+	flag.Parse()
+	if flag.NArg() == 0 {
+		// 如果没有参数默认从标准输入读取内容
+		cat(bufio.NewReader(os.Stdin))
+	}
+
+	// 依次读取每个指定文件的内容并打印到终端
+	for i := 0; i < flag.NArg(); i++ {
+		f, err := os.Open(flag.Arg(i))
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "reading from %s failed, err:%v\n", flag.Arg(i), err)
+			continue
+		}
+
+		cat(bufio.NewReader(f))
+	}
+}
