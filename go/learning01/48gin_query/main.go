e8dc00abfe66cb8b36b31306113ba7fe324f193d
bodii <1401097251@qq.com>
2020-10-05 21:54:26 +0800

    add gin package use params passing

---
+++ go/learning01/48gin_query/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getQueryDemo() {
+	r := gin.Default()
+	r.GET("/user/search", func(c *gin.Context) {
+		username := c.DefaultQuery("username", "jack")
+		address := c.Query("address")
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+	})
+
+	r.Run(":8000")
+}
+
+func postFormDemo() {
+	r := gin.Default()
+	r.POST("/user/search", func(c *gin.Context) {
+		username := c.PostForm("username")
+		address := c.PostForm("address")
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+	})
+
+	r.Run(":8000")
+}
+
+func getPathParamsDemo() {
+	r := gin.Default()
+	r.GET("/user/search/:username/:address", func(c *gin.Context) {
+		username := c.Param("username")
+		address := c.Param("address")
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+	})
+
+	r.Run(":8000")
+}
+
+type Login struct {
+	User     string `form:"user" json:"user" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+func paramBindingDemo() {
+	router := gin.Default()
+
+	router.POST("/loginJSON", func(c *gin.Context) {
+		var login Login
+
+		if err := c.ShouldBind(&login); err == nil {
+			fmt.Printf("login info:%#v\n", login)
+			c.JSON(http.StatusOK, gin.H{
+				"user":     login.User,
+				"password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+		}
+	})
+
+	router.POST("loginForm", func(c *gin.Context) {
+		var login Login
+		if err := c.ShouldBind(&login); err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"user":     login.User,
+				"password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	})
+
+	router.GET("/loginForm", func(c *gin.Context) {
+		var login Login
+		if err := c.ShouldBind(&login); err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"user":     login.User,
+				"Password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	})
+
+	router.Run(":8000")
+}
+
+func main() {
+	// getQueryDemo()
+	// postFormDemo()
+	// getPathParamsDemo()
+	paramBindingDemo()
+}
