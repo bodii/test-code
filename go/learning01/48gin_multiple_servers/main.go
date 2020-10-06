fbd0ecef44c4837a13eca71395ec1e8ba47c192b
bodii <1401097251@qq.com>
2020-10-06 23:49:00 +0800

    gin multiple servers test

---
+++ go/learning01/48gin_multiple_servers/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	g errgroup.Group
+)
+
+func router01() http.Handler {
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"code":    http.StatusOK,
+				"message": "server 01",
+			},
+		)
+	})
+
+	return r
+}
+
+func router02() http.Handler {
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"code":    http.StatusOK,
+				"message": "server 02",
+			},
+		)
+	})
+
+	return r
+}
+
+func multipleServersDemo() {
+	server01 := &http.Server{
+		Addr:         ":8001",
+		Handler:      router01(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server02 := &http.Server{
+		Addr:         ":8002",
+		Handler:      router02(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// 借助errgroup.Group或者自行开启两个goroutine分别启动两个服务
+	g.Go(func() error {
+		return server01.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		return server02.ListenAndServe()
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func main() {
+	multipleServersDemo()
+}
