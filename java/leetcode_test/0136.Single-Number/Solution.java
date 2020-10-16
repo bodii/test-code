1c53e7d01f9334ea307629d928bf75008b189ce0
bodii <1401097251@qq.com>
2020-10-16 23:38:39 +0800

    update single number

--- java/leetcode_test/0136.Single-Number/Solution.java
+++ java/leetcode_test/0136.Single-Number/Solution.java
@@ -2,12 +2,19 @@ package single_number;
 
 class Solution {
     public int singleNumber(int[] nums) {
+        /*
         int result = nums[0];
         for (int i = 1; i < nums.length; i ++) {
             System.out.printf("result:%d num:%d xor:%d\n", result, nums[i], result ^ nums[i]);
             result ^= nums[i];
         }
         return result;
+        */
+        for (int i = 1; i < nums.length; i ++) {
+            nums[0] ^= nums[i];
+        }
+        return nums[0];
+        
     }
 
     public static void main(String[] args) {
