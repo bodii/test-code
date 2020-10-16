ab83b86f9d606825206686ef33b715e6d8b210b8
bodii <1401097251@qq.com>
2020-10-16 23:24:01 +0800

    add single number

---
+++ java/leetcode_test/0136.Single-Number/Solution.java
@@ -0,0 +1,18 @@
+package single_number;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i ++) {
+            System.out.printf("result:%d num:%d xor:%d\n", result, nums[i], result ^ nums[i]);
+            result ^= nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        Solution s = new Solution();
+        System.out.println(s.singleNumber(nums));
+    }
+}
