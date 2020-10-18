79599acede315805027ace57203609c4ab1b0ffa
bodii <1401097251@qq.com>
2020-10-18 20:06:56 +0800

    add excel sheet coulumn number

---
+++ java/leetcode_test/0171.Excel-Sheet-Column-Number/Solution.java
@@ -0,0 +1,21 @@
+package excel_sheel_coulumn_number;
+
+class Solution {
+    public int titleToNumber(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 26 + (s.charAt(i) - 'A' + 1);
+        }
+
+        return result;
+    }
+
+    public int charToNumber(char c) {
+        return c - 'A' + 1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.titleToNumber("A"));
+    }
+}
