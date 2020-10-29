805cc421b86fa09d3dab3169e7bf8d038da7b409
bodii <1401097251@qq.com>
2020-10-29 23:36:56 +0800

    add Session struct

--- go/learning_orm/session/raw.go
+++ go/learning_orm/session/raw.go
@@ -1,1 +1,37 @@
 package session
+
+import (
+	"database/sql"
+	"strings"
+)
+
+// Session type struct
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+// New create Session instance
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+// Clear reset Session
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+// DB return db object
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+// Raw create sql content
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values)
+	return s
+}
