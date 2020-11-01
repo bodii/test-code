4f2966981274cb80ec1027c5e6f3bbbf0816440e
bodii <1401097251@qq.com>
2020-11-01 23:30:15 +0800

    add clause controllect sql

--- go/learning_orm/session/raw.go
+++ go/learning_orm/session/raw.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"database/sql"
+	"learning_orm/clause"
 	"learning_orm/dialect"
 	"learning_orm/log"
 	"learning_orm/schema"
@@ -13,6 +14,7 @@ type Session struct {
 	db       *sql.DB
 	dialect  dialect.Dialect
 	refTable *schema.Schema
+	clause   clause.Clause
 	sql      strings.Builder
 	sqlVars  []interface{}
 }
@@ -29,6 +31,7 @@ func New(db *sql.DB, dialect dialect.Dialect) *Session {
 func (s *Session) Clear() {
 	s.sql.Reset()
 	s.sqlVars = nil
+	s.clause = clause.Clause()
 }
 
 // DB return db object
