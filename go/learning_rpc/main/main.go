76ac0fc090aa7c51cc6ea5143c2572dfe22d0492
bodii <1401097251@qq.com>
2020-12-03 22:29:26 +0800

    add main test

--- go/learning_rpc/main/main.go
+++ go/learning_rpc/main/main.go
@@ -6,9 +6,13 @@ import (
 	"fmt"
 	"learning_rpc"
 	"learning_rpc/codec"
+	"learning_rpc/register"
 	"learning_rpc/xclient"
 	"log"
 	"net"
+	"net/http"
+	"reflect"
+	"strings"
 	"sync"
 	"time"
 )
@@ -25,19 +29,19 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-// func startServer(addr chan string) {
-// 	var foo Foo
-// 	if err := learning_rpc.Register(&foo); err != nil {
-// 		log.Fatal("register error:", err)
-// 	}
-// 	l, err := net.Listen("tcp", ":0")
-// 	if err != nil {
-// 		log.Fatal("network error:", err)
-// 	}
-// 	log.Println("start rpc server on", l.Addr())
-// 	addr <- l.Addr().String()
-// 	learning_rpc.Accept(l)
-// }
+func startServer(addr chan string) {
+	var foo Foo
+	if err := learning_rpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	learning_rpc.Accept(l)
+}
 
 func serverTest() {
 	addr := make(chan string)
@@ -64,104 +68,104 @@ func serverTest() {
 	}
 }
 
-// func clientTest() {
-// 	log.SetFlags(0)
-// 	addr := make(chan string)
-// 	go startServer(addr)
-// 	client, _ := learning_rpc.Dial("tcp", <-addr)
-// 	defer func() { _ = client.Close() }()
+func clientTest() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := learning_rpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
-// 	time.Sleep(time.Second)
+	time.Sleep(time.Second)
 
-// 	var wg sync.WaitGroup
-// 	for i := 0; i < 5; i++ {
-// 		wg.Add(1)
-// 		go func(i int) {
-// 			defer wg.Done()
-// 			args := fmt.Sprintf("learning_rpc req %d", i)
-// 			var reply string
-// 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
-// 				log.Fatal("call Foo.Sum error:", err)
-// 			}
-// 			log.Println("reply:", reply)
-// 		}(i)
-// 	}
-// 	wg.Wait()
-// }
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("learning_rpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
+	}
+	wg.Wait()
+}
 
-// func getMethodRecord() {
-// 	var wg sync.WaitGroup
-// 	typ := reflect.TypeOf(&wg)
-// 	for i := 0; i < typ.NumMethod(); i++ {
-// 		method := typ.Method(i)
-// 		argv := make([]string, 0, method.Type.NumIn())
-// 		returns := make([]string, 0, method.Type.NumOut())
-// 		// start from j, the 0th input is wg itself
-// 		for j := 1; j < method.Type.NumIn(); j++ {
-// 			argv = append(argv, method.Type.In(j).Name())
-// 		}
-// 		for j := 0; j < method.Type.NumOut(); j++ {
-// 			returns = append(returns, method.Type.Out(j).Name())
-// 		}
-// 		fmt.Printf("func (w *%s) %s(%s) %s\n",
-// 			typ.Elem().Name(),
-// 			method.Name,
-// 			strings.Join(argv, ","),
-// 			strings.Join(returns, ","))
-// 	}
-// }
+func getMethodRecord() {
+	var wg sync.WaitGroup
+	typ := reflect.TypeOf(&wg)
+	for i := 0; i < typ.NumMethod(); i++ {
+		method := typ.Method(i)
+		argv := make([]string, 0, method.Type.NumIn())
+		returns := make([]string, 0, method.Type.NumOut())
+		// start from j, the 0th input is wg itself
+		for j := 1; j < method.Type.NumIn(); j++ {
+			argv = append(argv, method.Type.In(j).Name())
+		}
+		for j := 0; j < method.Type.NumOut(); j++ {
+			returns = append(returns, method.Type.Out(j).Name())
+		}
+		fmt.Printf("func (w *%s) %s(%s) %s\n",
+			typ.Elem().Name(),
+			method.Name,
+			strings.Join(argv, ","),
+			strings.Join(returns, ","))
+	}
+}
 
-// func sendRPCRequest() {
-// 	log.SetFlags(0)
-// 	addr := make(chan string)
-// 	go startServer(addr)
-// 	client, _ := learning_rpc.Dial("tcp", <-addr)
-// 	defer func() { _ = client.Close() }()
+func sendRPCRequest() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := learning_rpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
-// 	time.Sleep(time.Second)
-// 	var wg sync.WaitGroup
-// 	for i := 0; i < 5; i++ {
-// 		wg.Add(1)
-// 		go func(i int) {
-// 			defer wg.Done()
-// 			args := &Args{Num1: i, Num2: i * i}
-// 			var reply int
-// 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
-// 				log.Fatal("call Foo.Sum error:", err)
-// 			}
-// 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
-// 		}(i)
-// 	}
-// 	wg.Wait()
-// }
+	time.Sleep(time.Second)
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
 
-// func call(addrCh chan string) {
-// 	client, _ := learning_rpc.DialHTTP("tcp", <-addrCh)
-// 	defer func() { _ = client.Close() }()
-// 	time.Sleep(time.Second)
+func call(addrCh chan string) {
+	client, _ := learning_rpc.DialHTTP("tcp", <-addrCh)
+	defer func() { _ = client.Close() }()
+	time.Sleep(time.Second)
 
-// 	var wg sync.WaitGroup
-// 	for i := 0; i < 5; i++ {
-// 		wg.Add(1)
-// 		go func(i int) {
-// 			defer wg.Done()
-// 			args := &Args{Num1: i, Num2: i * i}
-// 			var reply int
-// 			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
-// 				log.Fatal("call Foo.Sum error:", err)
-// 			}
-// 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
-// 		}(i)
-// 	}
-// 	wg.Wait()
-// }
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
 
-// func debug() {
-// 	log.SetFlags(0)
-// 	ch := make(chan string)
-// 	go call(ch)
-// 	startServer(ch)
-// }
+func debug() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call2(ch)
+	startServer(ch)
+}
 
 func (f Foo) Sleep(args Args, reply *int) error {
 	time.Sleep(time.Second * time.Duration(args.Num1))
@@ -169,7 +173,7 @@ func (f Foo) Sleep(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addrCh chan string) {
+func startServer2(addrCh chan string) {
 	var foo Foo
 	l, _ := net.Listen("tcp", ":0")
 	server := learning_rpc.NewServer()
@@ -194,7 +198,7 @@ func foo(xc *xclient.XClient, ctx context.Context, typ, serviceMethod string, ar
 	}
 }
 
-func call(addr1, addr2 string) {
+func call2(addr1, addr2 string) {
 	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
 	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
 	defer func() { _ = xc.Close() }()
@@ -210,7 +214,7 @@ func call(addr1, addr2 string) {
 	wg.Wait()
 }
 
-func broadcast(addr1, addr2 string) {
+func broadcast2(addr1, addr2 string) {
 	d := xclient.NewMultiServerDiscovery([]string{"tpc@" + addr1, "tcp@" + addr2})
 	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
 	defer func() { _ = xc.Close() }()
@@ -228,9 +232,58 @@ func broadcast(addr1, addr2 string) {
 	wg.Wait()
 }
 
-func main() {
-	// getMethodRecord()
-	// sendRPCRequest()
+func startRegistry(wg *sync.WaitGroup) {
+	l, _ := net.Listen("tcp", ":9999")
+	register.HandleHTTP()
+	wg.Done()
+	_ = http.Serve(l, nil)
+}
+
+func startServer03(registryAddr string, wg *sync.WaitGroup) {
+	var foo Foo
+	l, _ := net.Listen("tcp", ":0")
+	server := learning_rpc.NewServer()
+	_ = server.Register(&foo)
+	register.Heartbeat(registryAddr, "tcp@"+l.Addr().String(), 0)
+	wg.Done()
+	server.Accept(l)
+}
+
+func call3(registry string) {
+	d := xclient.NewLearningRegisteryDiscovery(registry, 0)
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "broadcast", "Foo.Sum", &Args{Num1: i, Num2: i * i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func broadcast3(registry string) {
+	d := xclient.NewLearningRegisteryDiscovery(registry, 0)
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "broadcast", "Foo.Sum", &Args{Num1: i, Num2: i * i})
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+			foo(xc, ctx, "broadcast", "Foo.Sleep", &Args{Num1: i, Num2: i * i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func test2() {
 	log.SetFlags(0)
 	ch1 := make(chan string)
 	ch2 := make(chan string)
@@ -242,6 +295,32 @@ func main() {
 	addr2 := <-ch2
 
 	time.Sleep(time.Second)
-	call(addr1, addr2)
-	broadcast(addr1, addr2)
+	call2(addr1, addr2)
+	broadcast2(addr1, addr2)
+}
+
+func test3() {
+	log.SetFlags(0)
+	registryAddr := "http://localhost:9999/_learning_rpc_/registry"
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go startRegistry(&wg)
+	wg.Wait()
+
+	time.Sleep(time.Second)
+	wg.Add(2)
+	go startServer03(registryAddr, &wg)
+	go startServer03(registryAddr, &wg)
+	wg.Wait()
+
+	time.Sleep(time.Second)
+	call3(registryAddr)
+	broadcast3(registryAddr)
+}
+
+func main() {
+	// getMethodRecord()
+	// sendRPCRequest()
+
+	test3()
 }
