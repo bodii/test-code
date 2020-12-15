2609cbdcde3f2470a29a7ed9ef8bb49ff11af469
bodii <1401097251@qq.com>
2020-12-15 18:13:51 +0800

    add context handle program

---
+++ go/learning_web_framework/web.go
@@ -0,0 +1,43 @@
+package learning_web_framework
+
+import "net/http"
+
+// HandlerFunc defines the request handler used by web
+type HandlerFunc func(http.ResponseWrite, *http.Request)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{
+		router: make(map[string]HandlerFunc)
+	}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func(engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
