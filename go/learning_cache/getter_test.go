b58f7c38fb877fb4843532559121e6fe0f06f747
bodii <1401097251@qq.com>
2020-12-29 09:10:42 +0800

    add test file

---
+++ go/learning_cache/getter_test.go
@@ -0,0 +1,18 @@
+package learningcache
+
+import (
+	"reflect"
+	"testing"
+)
+
+// TestGetter test getter function
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	expect := []byte("key")
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Errorf("callback failed")
+	}
+}
---
+++ go/learning_cache/group_test.go
@@ -0,0 +1,43 @@
+package learningcache
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+	g := NewGroup("score", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key]++
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	for k, v := range db {
+		if view, err := g.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		}
+
+		if _, err := g.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+
+	if view, err := g.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
+	}
+}
