733e00f51df75672274515de88a369a6702048eb
bodii <1401097251@qq.com>
2021-01-14 23:12:12 +0800

    add container example test

---
+++ go/go_programing_mode/go_generation/test001/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// Container is a generic container, accepting anything
+type Container []interface{}
+
+// Put adds an element to the container
+func (c *Container) Put(elem interface{}) {
+	*c = append(*c, elem)
+}
+
+// Get gets an element from the container.
+func (c *Container) Get() interface{} {
+	elem := (*c)[0]
+	*c = (*c)[1:]
+	return elem
+}
+
+func test01() {
+	intContainer := &Container{}
+	intContainer.Put(7)
+	intContainer.Put(42)
+
+	elem, ok := intContainer.Get().(int)
+	if !ok {
+		fmt.Println("Unable to read an int from intContainer")
+	}
+
+	fmt.Printf("assertExample: %d (%T)\n", elem, elem)
+}
+
+func main() {
+	test01()
+}
