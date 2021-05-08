0028ceda49c2dff33b035cd8e436eba2061b55eb
bodii <1401097251@qq.com>
2021-05-08 12:00:34 +0800

    add leetcode: 796 rotate string solution 03

---
+++ go/leetcode_go/0796.Rotate_String/Solution03/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func rotateString(A string, B string) bool {
+	return len(A) == len(B) && strings.Contains((A+A), B)
+}
+
+func test01() {
+	A, B := "abcde", "cdeab"
+	success := true
+
+	fmt.Println("test01 call func rotateString param A:=", A, " B:=", B)
+	fmt.Println("success result:", success)
+	fmt.Println("result:", rotateString(A, B))
+	fmt.Println()
+}
+
+func test02() {
+	A, B := "abcde", "abced"
+	success := false
+
+	fmt.Println("test02 call func rotateString param A:=", A, " B:=", B)
+	fmt.Println("success result:", success)
+	fmt.Println("result:", rotateString(A, B))
+	fmt.Println()
+}
+
+func test03() {
+	A, B := "eckadf", "kadfec"
+	success := true
+
+	fmt.Println("test03 call func rotateString param A:=", A, " B:=", B)
+	fmt.Println("success result:", success)
+	fmt.Println("result:", rotateString(A, B))
+	fmt.Println()
+}
+
+func test04() {
+	A, B := "bbbacddceeb", "ceebbbbacdd"
+	success := true
+	fmt.Println("test04 call func rotateString param A:=", A, " B:=", B)
+	fmt.Println("success result:", success)
+	fmt.Println("result:", rotateString(A, B))
+	fmt.Println()
+
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+	test04()
+}
