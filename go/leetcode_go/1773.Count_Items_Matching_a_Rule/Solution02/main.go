3f45054e026f066e08c5178a79c61ec71ab62f32
bodii <1401097251@qq.com>
2021-05-15 08:48:13 +0800

    add leetcode: 1773. count items matching a rule solution

---
+++ go/leetcode_go/1773.Count_Items_Matching_a_Rule/Solution02/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func countMatches(items [][]string, ruleKey string, ruleValue string) int {
+	ruleKeys := map[string]int{"type": 0, "color": 1, "name": 2}
+	index := ruleKeys[ruleKey]
+	nums := 0
+	for _, v := range items {
+		if v[index] == ruleValue {
+			nums++
+		}
+	}
+
+	return nums
+}
+
+func test01() {
+	items := [][]string{{"phone", "blue", "pixel"}, {"computer", "silver", "lenovo"}, {"phone", "gold", "iphone"}}
+	ruleKey := "color"
+	ruleValue := "silver"
+	succResult := 1
+	fmt.Println("test01 item:=", items)
+	fmt.Println("ruleKey:=", ruleKey, " ruleVlaue:=", ruleValue)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", countMatches(items, ruleKey, ruleValue))
+	fmt.Println()
+}
+
+func test02() {
+	items := [][]string{{"phone", "blue", "pixel"}, {"computer", "silver", "phone"}, {"phone", "gold", "iphone"}}
+	ruleKey := "type"
+	ruleValue := "phone"
+	succResult := 2
+	fmt.Println("test01 item:=", items)
+	fmt.Println("ruleKey:=", ruleKey, " ruleVlaue:=", ruleValue)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", countMatches(items, ruleKey, ruleValue))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+}
