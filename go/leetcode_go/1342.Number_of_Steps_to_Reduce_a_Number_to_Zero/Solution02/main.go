6f230d8c5c5e030122a18a280806e07bc889d383
bodii <1401097251@qq.com>
2021-06-02 08:36:45 +0800

    add leetcode: 1342. number of steps to reduce a number to zero solution

---
+++ go/leetcode_go/1342.Number_of_Steps_to_Reduce_a_Number_to_Zero/Solution02/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func numberOfSteps(num int) int {
+	count := 0
+	for num > 0 {
+		if (num & 1) == 0 {
+			num >>= 1
+		} else {
+			num ^= 1
+		}
+
+		count++
+	}
+
+	return count
+}
+
+func test01() {
+	num := 14
+	succResult := 6
+	fmt.Println("test01 num:=", num)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", numberOfSteps(num))
+	fmt.Println()
+}
+
+func test02() {
+	num := 8
+	succResult := 4
+	fmt.Println("test02 num:=", num)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", numberOfSteps(num))
+	fmt.Println()
+}
+
+func test03() {
+	num := 123
+	succResult := 12
+	fmt.Println("test03 num:=", num)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", numberOfSteps(num))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+}
