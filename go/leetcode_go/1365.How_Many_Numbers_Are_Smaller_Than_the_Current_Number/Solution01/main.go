a2b48eaf401a1d6af24bb852f5d974bc1230653a
bodii <1401097251@qq.com>
2021-06-03 07:25:13 +0800

    add leetcode: 1365. how many numbers are smaller than the current number solution

---
+++ go/leetcode_go/1365.How_Many_Numbers_Are_Smaller_Than_the_Current_Number/Solution01/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	result := make([]int, len(nums))
+	for i := range nums {
+		for j := range nums {
+			if i != j && nums[j] < nums[i] {
+				result[i]++
+			}
+		}
+	}
+
+	return result
+}
+
+func test01() {
+	nums := []int{8, 1, 2, 2, 3}
+	succResult := []int{4, 0, 1, 1, 3}
+	fmt.Println("test01 nums:=", nums)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", smallerNumbersThanCurrent(nums))
+	fmt.Println()
+}
+
+func test02() {
+	nums := []int{6, 5, 4, 8}
+	succResult := []int{2, 1, 0, 3}
+	fmt.Println("test01 nums:=", nums)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", smallerNumbersThanCurrent(nums))
+	fmt.Println()
+}
+
+func test03() {
+	nums := []int{7, 7, 7, 7}
+	succResult := []int{0, 0, 0, 0}
+	fmt.Println("test01 nums:=", nums)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", smallerNumbersThanCurrent(nums))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+}
---
+++ go/leetcode_go/1365.How_Many_Numbers_Are_Smaller_Than_the_Current_Number/Solution02/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	numsLen := len(nums)
+	result := make([]int, numsLen)
+	copyNums := make([]int, numsLen)
+	copy(copyNums, nums)
+
+	sort.Ints(copyNums)
+	for k, v := range nums {
+		result[k] = sort.SearchInts(copyNums, v)
+	}
+
+	return result
+}
+
+func test01() {
+	nums := []int{8, 1, 2, 2, 3}
+	succResult := []int{4, 0, 1, 1, 3}
+	fmt.Println("test01 nums:=", nums)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", smallerNumbersThanCurrent(nums))
+	fmt.Println()
+}
+
+func test02() {
+	nums := []int{6, 5, 4, 8}
+	succResult := []int{2, 1, 0, 3}
+	fmt.Println("test01 nums:=", nums)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", smallerNumbersThanCurrent(nums))
+	fmt.Println()
+}
+
+func test03() {
+	nums := []int{7, 7, 7, 7}
+	succResult := []int{0, 0, 0, 0}
+	fmt.Println("test01 nums:=", nums)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", smallerNumbersThanCurrent(nums))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+}
