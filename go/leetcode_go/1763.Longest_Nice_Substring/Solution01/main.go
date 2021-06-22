f3033b39c36e9586afedbbef5bdd51a036e6d1ee
bodii <1401097251@qq.com>
2021-06-22 18:33:51 +0800

    add leetcode: 1763. longest nice substring solution

--- go/leetcode_go/1763.Longest_Nice_Substring/Solution01/main.go
+++ go/leetcode_go/1763.Longest_Nice_Substring/Solution01/main.go
@@ -4,73 +4,117 @@ import (
 	"fmt"
 )
 
-func getNoZeroIntegers(n int) []int {
-	for i := 1; i <= n; i++ {
-		if isNoZero(i) && isNoZero(n-i) {
-			return []int{i, n - i}
+func longestNiceSubstring(s string) string {
+	sLen := len(s)
+	result := ""
+	for i := 0; i < sLen-1; i++ {
+		for j := i + 1; j < sLen; j++ {
+			if isNice(s, i, j) && len(result) < j-i+1 {
+				result = s[i : j+1]
+			}
 		}
 	}
-
-	return []int{}
+	return result
 }
 
-func isNoZero(n int) bool {
-	if n == 0 {
-		return false
+func isNice(s string, start, end int) bool {
+	lowers := make(map[byte]bool)
+	uppers := make(map[byte]bool)
+
+	for i := start; i <= end; i++ {
+		if s[i] >= 97 {
+			lowers[s[i]] = true
+		} else {
+			uppers[s[i]] = true
+		}
 	}
 
-	for n > 0 {
-		if n%10 == 0 {
+	for i := start; i <= end; i++ {
+		if (s[i] >= 97 && !uppers[s[i]-32]) || s[i] < 97 && !lowers[s[i]+32] {
 			return false
 		}
-		n /= 10
 	}
-
 	return true
 }
 
 func test01() {
-	n := 2
-	succResult := []int{1, 1}
-	fmt.Println("test01 n:=", n)
+	s := "YazaAay"
+	succResult := "aAa"
+	fmt.Println("test01 s:=", s)
 	fmt.Println("success result:=", succResult)
-	fmt.Println("result:=", getNoZeroIntegers(n))
+	fmt.Println("result:=", longestNiceSubstring(s))
 	fmt.Println()
 }
 
 func test02() {
-	n := 11
-	succResult := []int{2, 9}
-	fmt.Println("test02 n:=", n)
+	s := "Bb"
+	succResult := "Bb"
+	fmt.Println("test02 s:=", s)
 	fmt.Println("success result:=", succResult)
-	fmt.Println("result:=", getNoZeroIntegers(n))
+	fmt.Println("result:=", longestNiceSubstring(s))
 	fmt.Println()
 }
 
 func test03() {
-	n := 10000
-	succResult := []int{1, 9999}
-	fmt.Println("test03 n:=", n)
+	s := "c"
+	succResult := ""
+	fmt.Println("test03 s:=", s)
 	fmt.Println("success result:=", succResult)
-	fmt.Println("result:=", getNoZeroIntegers(n))
+	fmt.Println("result:=", longestNiceSubstring(s))
 	fmt.Println()
 }
 
 func test04() {
-	n := 69
-	succResult := []int{1, 68}
-	fmt.Println("test04 n:=", n)
+	s := "dDzeE"
+	succResult := "dD"
+	fmt.Println("test04 s:=", s)
 	fmt.Println("success result:=", succResult)
-	fmt.Println("result:=", getNoZeroIntegers(n))
+	fmt.Println("result:=", longestNiceSubstring(s))
 	fmt.Println()
 }
 
 func test05() {
-	n := 1010
-	succResult := []int{11, 999}
-	fmt.Println("test05 n:=", n)
+	s := "cChH"
+	succResult := "cChH"
+	fmt.Println("test05 s:=", s)
 	fmt.Println("success result:=", succResult)
-	fmt.Println("result:=", getNoZeroIntegers(n))
+	fmt.Println("result:=", longestNiceSubstring(s))
+	fmt.Println()
+}
+
+func test06() {
+	s := "dcChHfFKkv"
+	succResult := "cChHfFKk"
+	fmt.Println("test06 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", longestNiceSubstring(s))
+	fmt.Println()
+}
+
+func test07() {
+	s := "dcChHfFKkvaABbEeJjoOLl"
+	succResult := "aABbEeJjoOLl"
+	fmt.Println("test07 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", longestNiceSubstring(s))
+	fmt.Println()
+}
+
+func test08() {
+	s := "BebjJE"
+	succResult := "BebjJE"
+	fmt.Println("test08 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", longestNiceSubstring(s))
+	fmt.Println()
+}
+
+func test09() {
+	s := "qQUjJ"
+	succResult := "qQ"
+	fmt.Println("test09 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", longestNiceSubstring(s))
 	fmt.Println()
 }
 
@@ -80,4 +124,8 @@ func main() {
 	test03()
 	test04()
 	test05()
+	test06()
+	test07()
+	test08()
+	test09()
 }
