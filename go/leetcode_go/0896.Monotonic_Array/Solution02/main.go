9c485977829250dffd0b294b5758b05100bf4265
bodii <1401097251@qq.com>
2021-06-25 11:57:00 +0800

    add leetcode: 896. monotonic array soltuion

---
+++ go/leetcode_go/0896.Monotonic_Array/Solution02/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isMonotonic(nums []int) bool {
+	lastIndex := len(nums) - 1
+	isUp, isDown := true, true
+
+	for i := 0; i < lastIndex; i++ {
+		if nums[i] > nums[i+1] {
+			isUp = false
+		}
+
+		if nums[i] < nums[i+1] {
+			isDown = false
+		}
+	}
+
+	return isUp || isDown
+}
+
+func test01() {
+	prices := []int{1, 2, 2, 3}
+	succResult := true
+	fmt.Println("test01 prices:=", prices)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", isMonotonic(prices))
+	fmt.Println()
+}
+
+func test02() {
+	prices := []int{6, 5, 4, 4}
+	succResult := true
+	fmt.Println("test02 prices:=", prices)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", isMonotonic(prices))
+	fmt.Println()
+}
+
+func test03() {
+	prices := []int{1, 3, 2}
+	succResult := false
+	fmt.Println("test03 prices:=", prices)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", isMonotonic(prices))
+	fmt.Println()
+}
+
+func test04() {
+	prices := []int{1, 2, 4, 5}
+	succResult := true
+	fmt.Println("test04 prices:=", prices)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", isMonotonic(prices))
+	fmt.Println()
+}
+
+func test05() {
+	prices := []int{1, 1, 1}
+	succResult := true
+	fmt.Println("test05 prices:=", prices)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", isMonotonic(prices))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+	test04()
+	test05()
+}
