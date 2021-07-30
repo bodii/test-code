f2b362aafca08ad034c128f0ae5ceb4591454224
bodii <1401097251@qq.com>
2021-07-30 17:20:51 +0800

    add leetcode: 705 desing hashSet solution

---
+++ go/leetcode_go/0705.Design_HashSet/Solution01/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type MyHashSet struct {
+	data map[int]bool
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyHashSet {
+	return MyHashSet{make(map[int]bool)}
+}
+
+func (this *MyHashSet) Add(key int) {
+	this.data[key] = true
+}
+
+func (this *MyHashSet) Remove(key int) {
+	this.data[key] = false
+}
+
+/** Returns true if this set Contains the specified element */
+func (this *MyHashSet) Contains(key int) bool {
+	return this.data[key]
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
+
+func test01() {
+	myHashSet := Constructor()
+	myHashSet.Add(1)                   // set = [1]
+	myHashSet.Add(2)                   // set = [1, 2]
+	fmt.Println(myHashSet.Contains(1)) // 返回 True
+	fmt.Println(myHashSet.Contains(3)) // 返回 False ，（未找到）
+	myHashSet.Add(2)                   // set = [1, 2]
+	fmt.Println(myHashSet.Contains(2)) // 返回 True
+	myHashSet.Remove(2)                // set = [1]
+	fmt.Println(myHashSet.Contains(2)) // 返回 False ，（已移除）
+}
+
+func main() {
+	test01()
+}
