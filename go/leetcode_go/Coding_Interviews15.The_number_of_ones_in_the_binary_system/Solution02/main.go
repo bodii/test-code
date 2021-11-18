68412c066e804f519044b6a00ce2b0cef490d2fe
bodii <1401097251@qq.com>
2021-11-18 16:51:04 +0800

    add leetcode: coding interviews 15. the number of ones in the binary system solution

---
+++ go/leetcode_go/Coding_Interviews15.The_number_of_ones_in_the_binary_system/Solution02/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hammingWeight(num uint32) int {
+	count := 0
+
+	for num > 0 {
+		count += int(num & 1)
+		num >>= 1
+	}
+
+	return count
+}
+
+func test01() {
+	num := uint32(11)
+	succResult := 3
+	fmt.Println("test01 num:=", num)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", hammingWeight(num))
+	fmt.Println()
+}
+
+func test02() {
+	num := uint32(128)
+	succResult := 1
+	fmt.Println("test02 num:=", num)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", hammingWeight(num))
+	fmt.Println()
+}
+
+func test03() {
+	num := uint32(4294967293)
+	succResult := 31
+	fmt.Println("test03 num:=", num)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", hammingWeight(num))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+}
