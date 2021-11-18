ce02f27861178f00227c2073a7d31f6dee384fde
bodii <1401097251@qq.com>
2021-11-18 17:54:32 +0800

    add leetcode: 1356. sort integers by the number of 1 bits solution

---
+++ go/leetcode_go/1356.Sort_Integers_by_The_Number_of_1_Bits/Solution02/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"math/bits"
+	"sort"
+)
+
+func sortByBits(arr []int) []int {
+	sort.SliceStable(arr, func(i, j int) bool {
+		arrIBit1, arrJBit1 := bits.OnesCount32(uint32(arr[i])), bits.OnesCount32(uint32(arr[j]))
+
+		if arrIBit1 == arrJBit1 {
+			return arr[i] < arr[j]
+		}
+
+		return arrIBit1 < arrJBit1
+	})
+
+	return arr
+}
+
+func test01() {
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	succResult := []int{0, 1, 2, 4, 8, 3, 5, 6, 7}
+	fmt.Println("test01 arr:=", arr)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", sortByBits(arr))
+	fmt.Println()
+}
+
+func test02() {
+	arr := []int{1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1}
+	succResult := []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
+	fmt.Println("test02 arr:=", arr)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", sortByBits(arr))
+	fmt.Println()
+}
+
+func test03() {
+	arr := []int{10000, 10000}
+	succResult := []int{10000, 10000}
+	fmt.Println("test03 arr:=", arr)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", sortByBits(arr))
+	fmt.Println()
+}
+
+func test04() {
+	arr := []int{2, 3, 5, 7, 11, 13, 17, 19}
+	succResult := []int{2, 3, 5, 17, 7, 11, 13, 19}
+	fmt.Println("test04 arr:=", arr)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", sortByBits(arr))
+	fmt.Println()
+}
+
+func test05() {
+	arr := []int{10, 100, 1000, 10000}
+	succResult := []int{10, 100, 10000, 1000}
+	fmt.Println("test05 arr:=", arr)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", sortByBits(arr))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+	test04()
+	test05()
+}
