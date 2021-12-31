40744287e19bec492c707d19c38533ef127a2fb0
bodii <1401097251@qq.com>
2021-12-31 17:44:47 +0800

    add leetcode: 2027. minimum moves to convert string solution

---
+++ go/leetcode_go/2027.Minimum_Moves_to_Convert_String/Solution02/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+)
+
+func minimumMoves(s string) int {
+	sLen := len(s)
+
+	count := 0
+	for i := 0; i < sLen; i++ {
+		if s[i] == 'X' {
+			count++
+			i += 2
+		}
+	}
+
+	return count
+}
+
+func test01() {
+	s := "XXX"
+	succResult := 1
+	fmt.Println("test01 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", minimumMoves(s))
+	fmt.Println()
+}
+
+func test02() {
+	s := "XXOX"
+	succResult := 2
+	fmt.Println("test02 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", minimumMoves(s))
+	fmt.Println()
+}
+
+func test03() {
+	s := "OOOO"
+	succResult := 0
+	fmt.Println("test03 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", minimumMoves(s))
+	fmt.Println()
+}
+
+func test04() {
+	s := "OXOX"
+	succResult := 1
+	fmt.Println("test04 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", minimumMoves(s))
+	fmt.Println()
+}
+
+func test05() {
+	s := "OOXOOOX"
+	succResult := 2
+	fmt.Println("test05 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", minimumMoves(s))
+	fmt.Println()
+}
+
+func test06() {
+	s := "XXXOOXXX"
+	succResult := 2
+	fmt.Println("test06 s:=", s)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", minimumMoves(s))
+	fmt.Println()
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+	test04()
+	test05()
+	test06()
+}
