621c77f3d5546f0033e4e6741d3e87980bc6b381
bodii <1401097251@qq.com>
2022-01-04 22:36:39 +0800

    add leetcode: interview questions 08.10.color fill LCCI solution

--- go/leetcode_go/Interview_questions08.10.Color_Fill_LCCI/Solution01/main.go
+++ go/leetcode_go/Interview_questions08.10.Color_Fill_LCCI/Solution01/main.go
@@ -5,40 +5,31 @@ import (
 )
 
 func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
-
-	rr := len(image) - 1
-	cr := len(image[0]) - 1
-	irl, irr := sr, sr
-	jcl, jcr := sc, sc
-
-	for irl > 0 {
-		if image[irl-1] == image[irl] {
-			irl--
-			image[irl] = newColor
+	val := image[sr][sc]
+	rLen, cLen := len(image), len(image[0])
+	var dfsFill func(i int, j int)
+	dfsFill = func(i int, j int) {
+		if i < 0 || i >= rLen || j < 0 || j >= cLen {
+			return
 		}
+
+		if image[i][j] != val {
+			return
+		}
+
+		if image[i][j] == -1 {
+			return
+		}
+
+		image[i][j] = -1
+		dfsFill(i-1, j)
+		dfsFill(i+1, j)
+		dfsFill(i, j-1)
+		dfsFill(i, j+1)
+		image[i][j] = newColor
 	}
 
-	for irr < rr {
-		if image[irr+1] == image[irr] {
-			irr++
-			image[irr] = newColor
-		}
-	}
-
-	for jcl > 0 {
-		if image[jcl-1] == image[jcl] {
-			jcl--
-			image[jcl] = newColor
-		}
-	}
-
-	for jcr < cr {
-		if image[jcr+1] == image[jcr] {
-			jcr++
-			image[jcr] = newColor
-		}
-	}
-
+	dfsFill(sr, sc)
 	return image
 }
 
@@ -53,6 +44,18 @@ func test01() {
 	fmt.Println()
 }
 
+func test02() {
+	image := [][]int{{1, 1, 1, 0}, {1, 0, 1, 0}, {1, 0, 0, 1}, {0, 1, 0, 1}, {1, 1, 1, 1}, {0, 1, 1, 0}}
+	sr, sc := 2, 1
+	newColor := 2
+	succResult := [][]int{{1, 1, 1, 0}, {1, 2, 1, 0}, {1, 2, 2, 1}, {0, 1, 2, 1}, {1, 1, 1, 1}, {0, 1, 1, 0}}
+	fmt.Println("test02 image:=", image, " sr:=", sr, " sc:=", sc)
+	fmt.Println("success result:=", succResult)
+	fmt.Println("result:=", floodFill(image, sr, sc, newColor))
+	fmt.Println()
+}
+
 func main() {
 	test01()
+	test02()
 }
