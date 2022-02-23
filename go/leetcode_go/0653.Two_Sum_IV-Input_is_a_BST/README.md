f82ad5d297ee4e0c1c55368843bf7297d82dc7ed
bodii <1401097251@qq.com>
2022-02-23 22:36:44 +0800

    add leetcode: 653. two sum IV-input is a BST solution

---
+++ go/leetcode_go/0653.Two_Sum_IV-Input_is_a_BST/README.md
@@ -0,0 +1,50 @@
+### 653. Two Sum IV - Input is a BST
+Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+
+
+Example 1:
+
+	Input: root = [5,3,6,2,4,null,7], k = 9
+	Output: true
+
+Example 2:
+
+	Input: root = [5,3,6,2,4,null,7], k = 28
+	Output: false
+
+
+
+Constraints:
+
+* The number of nodes in the tree is in the range [1, 10^4].
+* -10^4 <= Node.val <= 10^4
+* root is guaranteed to be a valid binary search tree.
+* -10^5 <= k <= 10^5
+
+----
+
+### 653. 两数之和 IV - 输入 BST
+给定一个二叉搜索树 root 和一个目标结果 k，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+
+
+
+示例 1：
+	输入: root = [5,3,6,2,4,null,7], k = 9
+	输出: true
+
+示例 2：
+
+	输入: root = [5,3,6,2,4,null,7], k = 28
+	输出: false
+
+
+
+提示:
+
+* 二叉树的节点个数的范围是  [1, 10^4].
+* -10^4 <= Node.val <= 10^4
+* root 为二叉搜索树
+* -10^5 <= k <= 10^5
+
+
---
+++ go/leetcode_go/0653.Two_Sum_IV-Input_is_a_BST/Solution02/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BinaryTree struct {
+	root  *TreeNode
+	count int
+}
+
+func (tn *TreeNode) addNode(newNode *TreeNode) {
+	if tn.Val > newNode.Val {
+		if tn.Left == nil {
+			tn.Left = newNode
+		} else {
+			tn.Left.addNode(newNode)
+		}
+	} else {
+		if tn.Right == nil {
+			tn.Right = newNode
+		} else {
+			tn.Right.addNode(newNode)
+		}
+	}
+}
+
+func (bt *BinaryTree) add(val int) {
+	var newNode TreeNode
+	newNode.Val = val
+	if bt.root == nil {
+		bt.root = &newNode
+	} else {
+		bt.root.addNode(&newNode)
+	}
+	bt.count++
+}
+
+func (tn *TreeNode) printNode() {
+	fmt.Print(" ->val: ", tn.Val)
+	if tn.Left != nil {
+		tn.Left.printNode()
+		fmt.Print(" l: ", tn.Left.Val)
+	}
+
+	if tn.Right != nil {
+		tn.Right.printNode()
+		fmt.Print(" r: ", tn.Right.Val)
+	}
+	fmt.Println()
+}
+
+func (bt *BinaryTree) printTree() {
+	if bt.root == nil {
+		return
+	}
+
+	bt.root.printNode()
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findTarget(root *TreeNode, k int) bool {
+	values := make(map[int]bool)
+
+	var preOrder func(*TreeNode) bool
+	preOrder = func(root *TreeNode) bool {
+		if root == nil {
+			return false
+		}
+
+		if values[k-root.Val] {
+			return true
+		}
+
+		values[root.Val] = true
+		return preOrder(root.Left) || preOrder(root.Right)
+	}
+
+	return preOrder(root)
+}
+
+func test01() {
+	root := []int{5, 3, 6, 2, 4, 7}
+	k := 9
+	successResult := true
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test01: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", findTarget(rootTree.root, k))
+	fmt.Printf("\n\n")
+}
+
+func test02() {
+	root := []int{5, 3, 6, 2, 4, 7}
+	k := 28
+	successResult := false
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test02: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", findTarget(rootTree.root, k))
+	fmt.Printf("\n\n")
+}
+
+func main() {
+	test01()
+	test02()
+}
