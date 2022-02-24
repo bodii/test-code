7c6eb388219d4ba5a1141cf0d5deb6e6e1a050b1
bodii <1401097251@qq.com>
2022-02-24 22:50:38 +0800

    add leetcode: 94. binary tree inorder traversal solution

---
+++ go/leetcode_go/0094.Binary_Tree_Inorder_Traversal/README.md
@@ -0,0 +1,71 @@
+### 94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+	Input: root = [1,null,2,3]
+	Output: [1,3,2]
+
+Example 2:
+
+	Input: root = []
+	Output: []
+
+Example 3:
+
+	Input: root = [1]
+	Output: [1]
+
+
+
+Constraints:
+
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+----
+
+### 94. 二叉树的中序遍历
+给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+
+
+示例 1：
+
+	输入：root = [1,null,2,3]
+	输出：[1,3,2]
+
+示例 2：
+
+	输入：root = []
+	输出：[]
+
+示例 3：
+
+	输入：root = [1]
+	输出：[1]
+
+示例 4：
+
+	输入：root = [1,2]
+	输出：[2,1]
+
+示例 5：
+
+	输入：root = [1,null,2]
+	输出：[1,2]
+
+
+
+提示：
+
+* 树中节点数目在范围 [0, 100] 内
+* -100 <= Node.val <= 100
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
---
+++ go/leetcode_go/0094.Binary_Tree_Inorder_Traversal/Solution02/main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BinaryTree struct {
+	root  *TreeNode
+	count int
+}
+
+func (tn *TreeNode) addNode(newNode *TreeNode) {
+	if tn.Val > newNode.Val {
+		if tn.Left == nil {
+			tn.Left = newNode
+		} else {
+			tn.Left.addNode(newNode)
+		}
+	} else {
+		if tn.Right == nil {
+			tn.Right = newNode
+		} else {
+			tn.Right.addNode(newNode)
+		}
+	}
+}
+
+func (bt *BinaryTree) add(val int) {
+	var newNode TreeNode
+	newNode.Val = val
+	if bt.root == nil {
+		bt.root = &newNode
+	} else {
+		bt.root.addNode(&newNode)
+	}
+	bt.count++
+}
+
+func (tn *TreeNode) printNode() {
+	fmt.Print(" ->val: ", tn.Val)
+	if tn.Left != nil {
+		tn.Left.printNode()
+		fmt.Print(" l: ", tn.Left.Val)
+	}
+
+	if tn.Right != nil {
+		tn.Right.printNode()
+		fmt.Print(" r: ", tn.Right.Val)
+	}
+	fmt.Println()
+}
+
+func (bt *BinaryTree) printTree() {
+	if bt.root == nil {
+		return
+	}
+
+	bt.root.printNode()
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+
+	stack := list.New()
+	for root != nil || stack.Len() > 0 {
+		for root != nil {
+			stack.PushBack(root)
+			root = root.Left
+		}
+
+		elem := stack.Back()
+		stack.Remove(elem)
+
+		node := elem.Value.(*TreeNode)
+		result = append(result, node.Val)
+		root = node.Right
+	}
+
+	return result
+}
+
+func test01() {
+	root := []int{1, 2, 3}
+	successResult := []int{1, 3, 2}
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test01: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", inorderTraversal(rootTree.root))
+	fmt.Printf("\n\n")
+}
+
+func test02() {
+	root := []int{}
+	successResult := []int{}
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test02: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", inorderTraversal(rootTree.root))
+	fmt.Printf("\n\n")
+}
+
+func test03() {
+	root := []int{1}
+	successResult := []int{1}
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test03: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", inorderTraversal(rootTree.root))
+	fmt.Printf("\n\n")
+}
+
+func test04() {
+	root := []int{1, 2}
+	successResult := []int{2, 1}
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test04: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", inorderTraversal(rootTree.root))
+	fmt.Printf("\n\n")
+}
+
+func test05() {
+	root := []int{1, 2}
+	successResult := []int{2, 1}
+	rootTree := &BinaryTree{}
+
+	fmt.Println("test05: ")
+	fmt.Println("root :=", root)
+
+	fmt.Println("create tree1:")
+	for _, n := range root {
+		rootTree.add(n)
+	}
+	rootTree.printTree()
+
+	fmt.Println("tree postorder Traversal success result:", successResult)
+	fmt.Println("tree postorder Traversal result:", inorderTraversal(rootTree.root))
+	fmt.Printf("\n\n")
+}
+
+func main() {
+	test01()
+	test02()
+	test03()
+	test04()
+	test05()
+}
