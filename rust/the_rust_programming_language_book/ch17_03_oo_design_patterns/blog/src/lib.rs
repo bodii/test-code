6194021c2fdf3d7c1e791c34c385fcf8a23b150f
bodii <1401097251@qq.com>
2023-11-16 11:21:55 +0800

    add: design patterns

---
+++ rust/the_rust_programming_language_book/ch17_03_oo_design_patterns/blog/src/lib.rs
@@ -0,0 +1,87 @@
+pub struct  Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    // content方法默认返回一个空字符串slice
+    // 这意味着无需为Draft和PendingReview结构体实现了content了
+    #[allow(unused_variables)]
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    // 覆盖State trait的content方法并返回post.content的值
+    // 这里获取post的引用作为参数，并返回post的一部分的引用，
+    // 所以返回的引用的生命周期与post参数相关，需要生命周期标注
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
