c076b62ddfec23567b23a81f0bd3b871d8100ed8
bodii <1401097251@qq.com>
2023-12-15 15:58:13 +0800

    add: signals

---
+++ go/go_by_example/signals/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/signal"
+  "syscall"
+)
+
+func main() {
+  sigs := make(chan os.Signal, 1)
+
+  signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+  // signal.Notify(sigs, os.Interrupt)
+  done := make(chan bool, 1)
+
+  go func() {
+    sig := <- sigs
+    fmt.Println()
+    fmt.Println(sig)
+    done <- true
+  }()
+
+  fmt.Println("awaiting signal")
+  <-done
+  fmt.Println("exiting")
+} 
