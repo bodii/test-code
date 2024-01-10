895aec4f8bbff6e704b3851c61048d5dc4e979f7
bodii <1401097251@qq.com>
2024-01-10 16:44:33 +0800

    add: axum test

---
+++ rust/axum_test/global-404-hander/src/main.rs
@@ -0,0 +1,37 @@
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+#[tokio::main]
+async fn main() {
+    let filter_layer = EnvFilter::try_from_default_env()
+        // .unwrap_or_else(|_| "example_global_404_handler=debug".into());
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(handler));
+    //
+    let app = app.fallback(handler_404);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "nothing to see here")
+}
