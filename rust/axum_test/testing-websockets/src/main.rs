62eb98a2d64e90a675752fe40cdd7abd897c472a
bodii <1401097251@qq.com>
2024-01-15 13:56:01 +0800

    add: test websockets

---
+++ rust/axum_test/testing-websockets/src/main.rs
@@ -0,0 +1,123 @@
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::Response,
+    routing::get,
+    Router,
+};
+use futures::{Sink, SinkExt, Stream, StreamExt};
+
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app()).await.unwrap();
+}
+
+fn app() -> Router {
+    Router::new()
+        .route("/integration-testable", get(integration_testable_handler))
+        .route("/unit-testable", get(unit_testable_handler))
+}
+
+async fn integration_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(integration_testable_handler_socket)
+}
+
+async fn integration_testable_handler_socket(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        if let Message::Text(msg) = msg {
+            if socket
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
+}
+
+async fn unit_testable_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(|socket| {
+        let (write, read) = socket.split();
+        unit_testable_handler_socket(write, read)
+    })
+}
+
+async fn unit_testable_handler_socket<W, R>(mut write: W, mut read: R)
+where
+    W: Sink<Message> + Unpin,
+    R: Stream<Item = Result<Message, axum::Error>> + Unpin,
+{
+    while let Some(Ok(msg)) = read.next().await {
+        if let Message::Text(msg) = msg {
+            if write
+                .send(Message::Text(format!("You said: {msg}")))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::extract::ws::Message;
+    use core::panic;
+    use std::{
+        future::IntoFuture,
+        net::{Ipv4Addr, SocketAddr},
+    };
+    use tokio_tungstenite::tungstenite;
+
+    #[tokio::test]
+    async fn integration_test() {
+        let listener = tokio::net::TcpListener::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 0)))
+            .await
+            .unwrap();
+
+        let addr = listener.local_addr().unwrap();
+        tokio::spawn(axum::serve(listener, app()).into_future());
+
+        let (mut socket, _response) =
+            tokio_tungstenite::connect_async(format!("ws://{addr}/integration-testable"))
+                .await
+                .unwrap();
+        socket
+            .send(tungstenite::Message::text("foo"))
+            .await
+            .unwrap();
+
+        let msg = match socket.next().await.unwrap().unwrap() {
+            tungstenite::Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
+
+    #[tokio::test]
+    async fn unit_test() {
+        let (socket_write, mut test_rx) = futures::channel::mpsc::channel(1024);
+        let (mut test_tx, socket_read) = futures::channel::mpsc::channel(1024);
+
+        tokio::spawn(unit_testable_handler_socket(socket_write, socket_read));
+
+        test_tx
+            .send(Ok(Message::Text("foo".to_owned())))
+            .await
+            .unwrap();
+
+        let msg = match test_rx.next().await.unwrap() {
+            Message::Text(msg) => msg,
+            other => panic!("expected a text message but got {other:?}"),
+        };
+
+        assert_eq!(msg, "You said: foo");
+    }
+}
