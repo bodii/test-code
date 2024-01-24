e4ef89fc87aa0e944af360b82e229c2c9d59c152
bodii <1401097251@qq.com>
2024-01-24 14:48:50 +0800

    add: stream to file

---
+++ rust/axum_test/stream-to-file/src/main.rs
@@ -0,0 +1,133 @@
+use std::io;
+
+use axum::{
+    body::Bytes,
+    extract::{Multipart, Path, Request},
+    http::StatusCode,
+    response::{Html, Redirect},
+    routing::{get, post},
+    BoxError, Router,
+};
+use futures::{Stream, TryStreamExt};
+use tokio::{fs::File, io::BufWriter};
+use tokio_util::io::StreamReader;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+const UPLOADS_DIRECTORY: &str = "uploads";
+
+#[tokio::main]
+async fn main() {
+    subscriber();
+
+    tokio::fs::create_dir(UPLOADS_DIRECTORY)
+        .await
+        .expect("failed to create `{UPLOADS_DIRECTORY:?}` directory");
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app()).await.unwrap();
+}
+
+fn subscriber() {
+    let filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| "stream_to_file=debug".into());
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
+
+fn app() -> Router {
+    Router::new()
+        .route("/", get(show_form).post(accept_form))
+        .route("/file/:file_name", post(save_request_body))
+}
+
+async fn show_form() -> Html<&'static str> {
+    Html(
+        r#"
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Upload something!</title>
+            </head>
+            <body>
+                <form action="/" method="post" enctype="multipart/form-data">
+                    <div>
+                        <label>
+                            Upload file:
+                            <input type="file" name="file" multiple>
+                        </label>
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Upload files">
+                    </div>
+                </form>
+            </body>
+        </html>
+        "#,
+    )
+}
+
+async fn save_request_body(
+    Path(file_name): Path<String>,
+    request: Request,
+) -> Result<(), (StatusCode, String)> {
+    stream_to_file(&file_name, request.into_body().into_data_stream()).await
+}
+
+fn path_is_valid(path: &str) -> bool {
+    let path = std::path::Path::new(path);
+    let mut components = path.components().peekable();
+
+    if let Some(first) = components.peek() {
+        if !matches!(first, std::path::Component::Normal(_)) {
+            return false;
+        }
+    }
+
+    components.count() == 1
+}
+
+async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, String)>
+where
+    S: Stream<Item = Result<Bytes, E>>,
+    E: Into<BoxError>,
+{
+    if !path_is_valid(path) {
+        return Err((StatusCode::BAD_REQUEST, "Invalid path".to_owned()));
+    }
+
+    async {
+        let body_with_io_error = stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
+        let body_reader = StreamReader::new(body_with_io_error);
+        futures::pin_mut!(body_reader);
+
+        let path = std::path::Path::new(UPLOADS_DIRECTORY).join(path);
+        let mut file = BufWriter::new(File::create(path).await?);
+
+        tokio::io::copy(&mut body_reader, &mut file).await?;
+
+        Ok::<_, io::Error>(())
+    }
+    .await
+    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
+}
+
+async fn accept_form(mut multipart: Multipart) -> Result<Redirect, (StatusCode, String)> {
+    while let Ok(Some(field)) = multipart.next_field().await {
+        let file_name = if let Some(file_name) = field.file_name() {
+            file_name.to_owned()
+        } else {
+            continue;
+        };
+        stream_to_file(&file_name, field).await?;
+    }
+
+    Ok(Redirect::to("/"))
+}
