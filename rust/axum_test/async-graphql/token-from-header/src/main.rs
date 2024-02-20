e3b96cc84b279af229c1ab0c8757bda06360fc61
bodii <1401097251@qq.com>
2024-02-20 17:27:05 +0800

    add: async graphql token from header example

---
+++ rust/axum_test/async-graphql/token-from-header/src/main.rs
@@ -0,0 +1,68 @@
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig, ALL_WEBSOCKET_PROTOCOLS},
+    EmptyMutation, Schema,
+};
+use async_graphql_axum::{GraphQLProtocol, GraphQLRequest, GraphQLResponse, GraphQLWebSocket};
+use axum::{
+    extract::{State, WebSocketUpgrade},
+    http::HeaderMap,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use token::{on_connection_init, QueryRoot, SubscriptionRoot, Token, TokenSchema};
+use tokio::net::TcpListener;
+
+async fn graphql_payground() -> impl IntoResponse {
+    Html(playground_source(
+        GraphQLPlaygroundConfig::new("/").subscription_endpoint("/ws"),
+    ))
+}
+
+fn get_token_from_headers(headers: &HeaderMap) -> Option<Token> {
+    headers
+        .get("Token")
+        .and_then(|value| value.to_str().map(|s| Token(s.to_string())).ok())
+}
+
+async fn graphql_handler(
+    State(schema): State<TokenSchema>,
+    headers: HeaderMap,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    let mut req = req.into_inner();
+    if let Some(token) = get_token_from_headers(&headers) {
+        req = req.data(token);
+    }
+    schema.execute(req).await.into()
+}
+
+async fn graphql_ws_handler(
+    State(schema): State<TokenSchema>,
+    protocol: GraphQLProtocol,
+    websocket: WebSocketUpgrade,
+) -> Response {
+    websocket
+        .protocols(ALL_WEBSOCKET_PROTOCOLS)
+        .on_upgrade(move |stream| {
+            GraphQLWebSocket::new(stream, schema.clone(), protocol)
+                .on_connection_init(on_connection_init)
+                .serve()
+        })
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::new(QueryRoot, EmptyMutation, SubscriptionRoot);
+
+    let app = Router::new()
+        .route("/", get(graphql_payground).post(graphql_handler))
+        .route("/ws", get(graphql_ws_handler))
+        .with_state(schema);
+
+    println!("Playground: http://localhost:3000");
+
+    axum::serve(TcpListener::bind("127.0.0.1:3000").await.unwrap(), app)
+        .await
+        .unwrap();
+}
